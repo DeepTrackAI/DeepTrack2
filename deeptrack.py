7057cec557186c7ca68edd0fc2d3109df716a9dc
Saga Helgadottir <saga.helgadottir@physics.gu.se>
2019-03-11 13:53:14 +0100

    Updated version for Optica (#11)
    * Made it so that the image_parameters in get_image_parameters hold on to the functions instead of the function's return values.
    
    In the get_image_generator() function there is now a single assignment of get_image_parameters into a dictionary of lambda functions that are used to update the image parameters each iteration.
    
    * Update deeptrack.py
    
    -Track non-square videos
    -Fixed x,y in plot_tracked_scanning_boxes
    -added option for elliptical particles in training
    -fixed the resizing of the boxes in track_video (track_frame)
    -added an option for preprocessing the images before tracking (normalise and enhance)
    
    * Video files working in vlc
    
    It seems that the issue with running the files in vlc was that the frame rate was too low. I increased the frame rate to 5 fps for all videos and tested in vls.
    
    * Edit example notebooks
    
    Edit example jupyter notebooks according to the changes made in deeptrack.py
    
    * deeptrack.py updated again
    
    in track_video and show_tracked_frames I took out the limit of showing maximum 10 frames since it interferes with the calculation of centroids of particles.
    
    * All files updated
    
    The updated files before resubmission in Optica.
    
    * All the updated videos
    
    The video updates for resubmission for Optica

--- deeptrack.py
+++ deeptrack.py
@@ -15,7 +15,9 @@ def get_image_parameters(
     image_background_level=lambda : .5,
     signal_to_noise_ratio=lambda : 30,
     gradient_intensity=lambda : .2, 
-    gradient_direction=lambda : 0):
+    gradient_direction=lambda : 0,
+    ellipsoidal_orientation=lambda : [0, ], 
+    ellipticity=lambda : 1):
     """Get image parameters.
     
     Inputs:
@@ -29,9 +31,11 @@ def get_image_parameters(
     signal_to_noise_ratio: signal to noise ratio [positive real number]
     gradient_intensity: gradient intensity [real number normalized to 1]
     gradient_direction: gradient angle [rad, real number]
+    ellipsoidal_orientation: Orientation of elliptical particles [rad, real number] 
+    ellipticity: shape of the particles, from spherical to elliptical [real number]
     
     Note: particle_center_x, particle_center_x, particle_radius, 
-    particle_bessel_order, particle_intensity must have the same length.
+    particle_bessel_order, particle_intensity, ellipsoidal_orientation must have the same length.
     
     Output:
     image_parameters: list with the values of the image parameters in a dictionary:
@@ -45,6 +49,8 @@ def get_image_parameters(
         image_parameters['Signal to Noise Ratio']
         image_parameters['Gradient Intensity']
         image_parameters['Gradient Direction']
+        image_parameters['Ellipsoid Orientation']
+        image_parameters['Ellipticity']
     """
     
     image_parameters = {}
@@ -58,6 +64,8 @@ def get_image_parameters(
     image_parameters['Signal to Noise Ratio'] = signal_to_noise_ratio()
     image_parameters['Gradient Intensity'] = gradient_intensity()
     image_parameters['Gradient Direction'] = gradient_direction()
+    image_parameters['Ellipsoid Orientation'] = ellipsoidal_orientation()
+    image_parameters['Ellipticity'] = ellipticity()
 
     return image_parameters
 
@@ -76,6 +84,8 @@ def generate_image(image_parameters):
         image_parameters['Signal to Noise Ratio']
         image_parameters['Gradient Intensity']
         image_parameters['Gradient Direction']
+        image_parameters['Ellipsoid Orientation']
+        image_parameters['Ellipticity']
         
     Note: image_parameters is typically obained from the function get_image_parameters()
         
@@ -83,7 +93,7 @@ def generate_image(image_parameters):
     image: image of the particle [2D numpy array of real numbers betwen 0 and 1]
     """
     
-    from numpy import meshgrid, arange, ones, zeros, sin, cos, sqrt, clip
+    from numpy import meshgrid, arange, ones, zeros, sin, cos, sqrt, clip, array
     from scipy.special import jv as bessel
     from numpy.random import poisson as poisson
     
@@ -97,6 +107,8 @@ def generate_image(image_parameters):
     signal_to_noise_ratio = image_parameters['Signal to Noise Ratio']
     gradient_intensity = image_parameters['Gradient Intensity']
     gradient_direction = image_parameters['Gradient Direction']
+    ellipsoidal_orientation_list = image_parameters['Ellipsoid Orientation']
+    ellipticity = image_parameters['Ellipticity']
     
     ### CALCULATE IMAGE PARAMETERS
     # calculate image full size
@@ -119,16 +131,25 @@ def generate_image(image_parameters):
 
     ### CALCULATE IMAGE PARTICLES
     image_particles = zeros((image_size, image_size))
-    for particle_center_x, particle_center_y, particle_radius, particle_bessel_orders, particle_intensities in zip(particle_center_x_list, particle_center_y_list, particle_radius_list, particle_bessel_orders_list, particle_intensities_list):
+    for particle_center_x, particle_center_y, particle_radius, particle_bessel_orders, particle_intensities, ellipsoidal_orientation in zip(particle_center_x_list, particle_center_y_list, particle_radius_list, particle_bessel_orders_list, particle_intensities_list, ellipsoidal_orientation_list):
         # calculate the radial distance from the center of the particle 
         # normalized by the particle radius
         radial_distance_from_particle = sqrt((image_coordinate_x - particle_center_x)**2 
                                          + (image_coordinate_y - particle_center_y)**2 
                                          + .001**2) / particle_radius
+        
+        # for elliptical particles
+        rotated_distance_x = (image_coordinate_x - particle_center_x)*cos(ellipsoidal_orientation) + (image_coordinate_y - particle_center_y)*sin(ellipsoidal_orientation)
+        rotated_distance_y = -(image_coordinate_x - particle_center_x)*sin(ellipsoidal_orientation) + (image_coordinate_y - particle_center_y)*cos(ellipsoidal_orientation)
+        
+        
+        elliptical_distance_from_particle = sqrt((rotated_distance_x)**2 
+                                         + (rotated_distance_y / ellipticity)**2 
+                                         + .001**2) / particle_radius
 
         # calculate particle profile
         for particle_bessel_order, particle_intensity in zip(particle_bessel_orders, particle_intensities):
-            image_particle = 4 * particle_bessel_order**2.5 * (bessel(particle_bessel_order, radial_distance_from_particle) / radial_distance_from_particle)**2
+            image_particle = 4 * particle_bessel_order**2.5 * (bessel(particle_bessel_order, elliptical_distance_from_particle) / elliptical_distance_from_particle)**2
             image_particles = image_particles + particle_intensity * image_particle
 
     # calculate image without noise as background image plus particle image
@@ -160,6 +181,8 @@ def get_image_generator(image_parameters_function=lambda : get_image_parameters(
         image_parameters['Signal to Noise Ratio']
         image_parameters['Gradient Intensity']
         image_parameters['Gradient Direction']
+        image_parameters['Ellipsoid Orientation']
+        image_parameters['Ellipticity']
     """    
     
     image_number = 0
@@ -195,6 +218,8 @@ def plot_sample_image(image, image_parameters, figsize=(15,5)):
     signal_to_noise_ratio = image_parameters['Signal to Noise Ratio']
     gradient_intensity = image_parameters['Gradient Intensity']
     gradient_direction = image_parameters['Gradient Direction']
+    ellipsoidal_orientation_list = image_parameters['Ellipsoid Orientation']
+    ellipticity = image_parameters['Ellipticity']
 
     plt.figure(figsize=figsize)
 
@@ -211,6 +236,8 @@ def plot_sample_image(image, image_parameters, figsize=(15,5)):
     plt.text(0, .7, 'particle radius = %5.2f px' % particle_radius_list[0], fontsize=16)
     plt.text(0, .6, 'Bessel order = %5.2f' % particle_bessel_orders_list[0][0], fontsize=16)
     plt.text(0, .5, 'particle intensity = %5.2f' % particle_intensities_list[0][0], fontsize=16)
+    plt.text(0, .4, 'ellipsoidal_orientation = %5.2f' % ellipsoidal_orientation_list[0], fontsize=16)
+    plt.text(0, .3, 'ellipticity = %5.2f' % ellipticity, fontsize=16)
     plt.axis('off')
 
     plt.subplot(1, 3, 3)
@@ -283,6 +310,7 @@ def create_deep_learning_network(
         
     # OUTPUT LAYER
     number_of_outputs = 3
+    
     output_layer = layers.Dense(number_of_outputs, name='output')
     network.add(output_layer)
     
@@ -333,6 +361,8 @@ def train_deep_learning_network(
     import numpy as np
     from time import time
     
+    number_of_outputs = 3
+    
     training_history = {}
     training_history['Sample Size'] = []
     training_history['Iteration Number'] = []
@@ -352,7 +382,7 @@ def train_deep_learning_network(
             input_shape = (sample_size, image_shape[0], image_shape[1], image_shape[2])
             images = np.zeros(input_shape)
             
-            output_shape = (sample_size, 3)
+            output_shape = (sample_size, number_of_outputs)
             targets = np.zeros(output_shape)
             
             for image_number, image, image_parameters in image_generator():
@@ -365,6 +395,7 @@ def train_deep_learning_network(
                 half_image_size = (image_shape[0] - 1) / 2
                 particle_center_x = image_parameters['Particle Center X List'][0]
                 particle_center_y = image_parameters['Particle Center Y List'][0]
+                
                 targets[image_number] = [particle_center_x / half_image_size,
                                          particle_center_y / half_image_size,
                                          (particle_center_x**2 + particle_center_y**2)**.5 / half_image_size]
@@ -453,9 +484,12 @@ def predict(network, image):
     predicted_position = network.predict(reshape(resized_image, (1, image_shape[0], image_shape[1], image_shape[2])))
 
     half_image_size = (image_shape[0] - 1) / 2
+   
     predicted_position = half_image_size * predicted_position[0]
+   
         
     return predicted_position
+    
 
 def plot_prediction(image, image_parameters, predicted_position, figsize=(15, 5)):
     """Plot a sample image.
@@ -483,10 +517,13 @@ def plot_prediction(image, image_parameters, predicted_position, figsize=(15, 5)
     signal_to_noise_ratio = image_parameters['Signal to Noise Ratio']
     gradient_intensity = image_parameters['Gradient Intensity']
     gradient_direction = image_parameters['Gradient Direction']
+    ellipsoidal_orientation_list = image_parameters['Ellipsoid Orientation']
+    ellipticity = image_parameters['Ellipticity']
     
     predicted_position_x = predicted_position[0]
     predicted_position_y = predicted_position[1]
     predicted_position_r = predicted_position[2]
+   
 
     plt.figure(figsize=figsize)
 
@@ -500,11 +537,14 @@ def plot_prediction(image, image_parameters, predicted_position, figsize=(15, 5)
     plt.ylabel('x (px)', fontsize=16)
 
     subplot131_handle = plt.subplot(1, 3, 2)
-    plt.text(0, .9, 'particle center x = %5.2f px' % particle_center_x_list[0], fontsize=16)
-    plt.text(0, .8, 'particle center y = %5.2f px' % particle_center_y_list[0], fontsize=16)
-    plt.text(0, .7, 'particle radius = %5.2f px' % particle_radius_list[0], fontsize=16)
-    plt.text(0, .6, 'Bessel order = %5.2f' % particle_bessel_orders_list[0][0], fontsize=16)
-    plt.text(0, .5, 'particle intensity = %5.2f' % particle_intensities_list[0][0], fontsize=16)
+    plt.text(0, 1, 'particle center x = %5.2f px' % particle_center_x_list[0], fontsize=16)
+    plt.text(0, .9, 'particle center y = %5.2f px' % particle_center_y_list[0], fontsize=16)
+    plt.text(0, .8, 'particle radius = %5.2f px' % particle_radius_list[0], fontsize=16)
+    plt.text(0, .7, 'Bessel order = %5.2f' % particle_bessel_orders_list[0][0], fontsize=16)
+    plt.text(0, .6, 'particle intensity = %5.2f' % particle_intensities_list[0][0], fontsize=16)
+    plt.text(0, .5, 'ellipsoidal_orientation = %5.2f' % ellipsoidal_orientation_list[0], fontsize=16)
+    plt.text(0, .4, 'ellipticity = %5.2f' % ellipticity, fontsize=16)
+    
     plt.text(0, .3, 'predicted x = %5.2f px' % predicted_position_x, fontsize=16, color='#e6661a')
     plt.text(0, .2, 'predicted y = %5.2f px' % predicted_position_y, fontsize=16, color='#e6661a')
     plt.text(0, .1, 'predicted r = %5.2f px' % predicted_position_r, fontsize=16, color='#e6661a')
@@ -541,6 +581,7 @@ def track_frame(
     """  
 
     import numpy as np
+    import cv2
     
     frame_width = frame.shape[1]
     frame_height = frame.shape[0]
@@ -576,9 +617,13 @@ def track_frame(
             # Define the scanning box
             boxes[j, k] = frame[int(box_center_x[j] - box_half_size):int(box_center_x[j] + box_half_size + 1), 
                                 int(box_center_y[k] - box_half_size):int(box_center_y[k] + box_half_size + 1)]
-
+            
+            box_predict = boxes[j, k]
+            
+            box_predict = cv2.resize(boxes[j, k], (51, 51))
+            
             # Predict position of particle with respect to the scanning box
-            prediction_wrt_box[j, k] = network.predict(np.reshape(boxes[j, k], (1, 51, 51, 1)))
+            prediction_wrt_box[j, k] = network.predict(np.reshape(box_predict, (1, 51, 51, 1)))
 
             prediction_wrt_box[j, k][0] = prediction_wrt_box[j, k][0] * box_half_size + box_half_size
             prediction_wrt_box[j, k][1] = prediction_wrt_box[j, k][1] * box_half_size + box_half_size
@@ -596,7 +641,8 @@ def track_video(
     number_frames_to_be_tracked=1,
     box_half_size=25,
     box_scanning_step=5,
-    ):
+    frame_normalize=0,
+    frame_enhance=1):
     """Track multiple particles in a video.
     
     Inputs:    
@@ -604,13 +650,16 @@ def track_video(
     network: the pre-trained network
     number_frames_to_be_tracked: number of frames to by analyzed from video begining. If number_frames is equal to 0 then the whole video is tracked.
     box_half_size: half the size of the scanning box. If box_half_size is equal to 0 then a single particle is tracked in a frame.
-    box_scanning_step: the size of the scanning step 
+    box_scanning_step: the size of the scanning step
+    frame_normalize: option to normalize the frame before tracking.
+    frame_enhance: option to enhance the frame before tracking.
     
     Output:
     frames: frames from video
     predicted_positions_wrt_frame: x, y and r coordinates with respect to the all frames (pixels) 
     predicted_positions_wrt_box: x, y and r coordinates with respect to the boxes for all frames (pixels) 
     boxes_all: the part of the frame corresponding to each box for all frames
+    number_frames_to_be_tracked: the number of frames that have been tracked. 
     """
     
     import cv2
@@ -626,7 +675,7 @@ def track_video(
     video_width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
 
     # Initialize variables
-    frames = np.zeros((min(number_frames_to_be_tracked,10), video_height, video_width))
+    frames = np.zeros((number_frames_to_be_tracked, video_height, video_width))
 
    
     box_center_x = np.arange(box_half_size, 
@@ -659,17 +708,20 @@ def track_video(
         (ret, frame) = video.read()
         
         # Normalize the frame
-        frame = cv2.normalize(frame, None, 0, 255, cv2.NORM_MINMAX)
+        if frame_normalize == 1:
+            frame = cv2.normalize(frame, None, 0, 255, cv2.NORM_MINMAX)
 
         # Convert color image to grayscale.
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) / 255
+        
+        frame = frame * frame_enhance
 
         # Generate the scanning boxes and predict particle position in each box
 
         (prediction_wrt_box, prediction_wrt_frame, boxes) = track_frame(network, frame, box_half_size, box_scanning_step)
 
-        if i < 10:
-            frames[i] = frame
+
+        frames[i] = frame
             
         predicted_positions_wrt_box[i] = prediction_wrt_box
         predicted_positions_wrt_frame[i] = prediction_wrt_frame
@@ -677,8 +729,9 @@ def track_video(
 
     # Release the video
     video.release()
+    
 
-    return (frames, predicted_positions_wrt_frame, predicted_positions_wrt_box, boxes_all)
+    return (number_frames_to_be_tracked, frames, predicted_positions_wrt_frame, predicted_positions_wrt_box, boxes_all)
 
 def plot_tracked_scanning_boxes(
     frame_to_be_shown,
@@ -702,21 +755,14 @@ def plot_tracked_scanning_boxes(
     """
 
     import matplotlib.pyplot as plt
-    
+       
     plt.figure(10)
 
     for i in list(frame_to_be_shown):
         for j in list(rows_to_be_shown):
             for k in list(columns_to_be_shown):
 
-                print ('frame', i)
-                print ('row', j)
-                print ('column', k)
-
-                print ('particle center x = %5.2f px' % predicted_positions_wrt_box[i, j, k, 0])
-                print ('particle center y = %5.2f px' % predicted_positions_wrt_box[i, j, k, 1])
-                print ('particle radius = %5.2f px' % predicted_positions_wrt_box[i, j, k, 2])
-
+                plt.subplot(1, 2, 1)
                 plt.imshow(boxes_all[i, j, k],
                            cmap='gray', 
                            vmin=0, 
@@ -725,7 +771,25 @@ def plot_tracked_scanning_boxes(
                 plt.plot(predicted_positions_wrt_box[i, j, k, 1],
                          predicted_positions_wrt_box[i, j, k, 0], 
                          'ob')
+                plt.xlabel('y (px)', fontsize=16)
+                plt.ylabel('x (px)', fontsize=16)
+                
+                
+                plt.subplot(1, 2, 2)
 
+                plt.text(0, .8, 'frame = %1.0f' % i, fontsize=16)
+                plt.text(0, .7, 'row = %1.0f' % j, fontsize=16)
+                plt.text(0, .6, 'column = %1.0f' % k, fontsize=16)
+                
+                plt.text(0, .4, 'particle center x = %5.2f px' % predicted_positions_wrt_box[i, j, k, 0], 
+                         fontsize=16, color='b')
+                plt.text(0, .3, 'particle center y = %5.2f px' % predicted_positions_wrt_box[i, j, k, 1], 
+                         fontsize=16, color='b')
+                plt.text(0, .2, 'particle radius = %5.2f px' % predicted_positions_wrt_box[i, j, k, 2], 
+                         fontsize=16, color='b')
+                plt.axis('off')
+
+                
                 plt.show()
 
 def centroids(
@@ -862,11 +926,15 @@ def show_tracked_frames(
             fillstyle='none',
             markersize=10,
             )
+        
+    return #(particle_positions, particle_centroids)
 
 def track_video_single_particle(
     video_file_name,
     network,
-    number_frames_to_be_tracked=1):
+    number_frames_to_be_tracked=1,
+    frame_normalize=0,
+    frame_enhance=1):
     """Track single particlee in a video.
     
     Inputs:    
@@ -892,7 +960,7 @@ def track_video_single_particle(
     video_height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
     # Initialize variables
-    frames = np.zeros((min(number_frames_to_be_tracked,10), video_height, video_width))
+    frames = np.zeros((number_frames_to_be_tracked, video_height, video_width))
 
     predicted_positions = np.zeros((number_frames_to_be_tracked, 3))
 
@@ -903,10 +971,14 @@ def track_video_single_particle(
         (ret, frame) = video.read()
         
         # Normalize the frame
-        frame = cv2.normalize(frame, None, 0, 255, cv2.NORM_MINMAX)
+        if frame_normalize == 1:
+            frame = cv2.normalize(frame, None, 0, 255, cv2.NORM_MINMAX)
 
         # Convert color image to grayscale.
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) / 255
+        
+        # Enhance the frame
+        frame = frame * frame_enhance
         
         ### Resize the frame
         frame_resize = cv2.resize(frame, (51, 51))
@@ -922,6 +994,7 @@ def track_video_single_particle(
         predicted_positions[i, 1] = predicted_position_y
         predicted_positions[i, 2] = predicted_position_r
     
+       
         if i < 10:
             frames[i] = frame
         
@@ -929,7 +1002,7 @@ def track_video_single_particle(
     # Release the video
     video.release()
 
-    return (frames, predicted_positions)
+    return (number_frames_to_be_tracked, frames, predicted_positions)
 
 def show_tracked_frames_single_particle(
     number_frames_to_be_shown,
