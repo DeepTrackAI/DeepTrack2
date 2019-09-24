a620aac97b0806931d7fcbdfb3216772ed06a5b4
Benjamin Midtvedt <benmid@student.chalmers.se>
2019-09-24 16:11:53 +0200

    Added the deeptrack model

---
+++ DeepTrack/Models.py
@@ -0,0 +1,69 @@
+from tensorflow.keras import models, layers
+
+def DeepTrackNetwork(
+    input_shape = (51, 51, 1),
+    conv_layers_dimensions = (16, 32, 64, 128), 
+    dense_layers_dimensions = (32, 32),
+    number_of_outputs = 3):
+    """Creates and compiles a deep learning network.
+    
+    Inputs:    
+    input_shape: size of the images to be analyzed [3-ple of positive integers, x-pixels by y-pixels by color channels]
+    conv_layers_dimensions: number of convolutions in each convolutional layer [tuple of positive integers]
+    dense_layers_dimensions: number of units in each dense layer [tuple of positive integers]
+        
+    Output:
+    network: deep learning network
+    """    
+
+    
+    
+    ### INITIALIZE DEEP LEARNING NETWORK
+    network = models.Sequential()
+
+    ### CONVOLUTIONAL BASIS
+    for conv_layer_number, conv_layer_dimension in zip(range(len(conv_layers_dimensions)), conv_layers_dimensions):
+
+        # add convolutional layer
+        conv_layer_name = 'conv_' + str(conv_layer_number + 1)
+        if conv_layer_number==0:
+            conv_layer = layers.Conv2D(conv_layer_dimension, 
+                                       (3, 3), 
+                                       activation='relu', 
+                                       input_shape=input_shape, 
+                                       name=conv_layer_name)
+        else:
+            conv_layer = layers.Conv2D(conv_layer_dimension, 
+                                       (3, 3), 
+                                       activation='relu', 
+                                       name=conv_layer_name)
+        network.add(conv_layer)
+
+        # add pooling layer
+        pooling_layer_name = 'pooling_' + str(conv_layer_number+1)
+        pooling_layer = layers.MaxPooling2D(2, 2, name=pooling_layer_name)
+        network.add(pooling_layer)
+    
+    # FLATTENING
+    flatten_layer_name = 'flatten'
+    flatten_layer = layers.Flatten(name=flatten_layer_name)
+    network.add(flatten_layer)
+    
+    # DENSE TOP
+    for dense_layer_number, dense_layer_dimension in zip(range(len(dense_layers_dimensions)), dense_layers_dimensions):
+        
+        # add dense layer
+        dense_layer_name = 'dense_' + str(dense_layer_number + 1)
+        dense_layer = layers.Dense(dense_layer_dimension, 
+                                   activation='relu', 
+                                   name=dense_layer_name)
+        network.add(dense_layer)
+        
+    # OUTPUT LAYER
+    
+    output_layer = layers.Dense(number_of_outputs, name='output')
+    network.add(output_layer)
+    
+    network.compile(optimizer='rmsprop', loss='mse', metrics=['mse', 'mae'])
+    
+    return network
--- Tests/Training_with_augmentation.py
+++ Tests/Training_with_augmentation.py
@@ -2,27 +2,35 @@
 
 
 import sys
+from timeit import default_timer as timer
+
+import matplotlib.pyplot as plt
+import numpy as np
+from tensorflow import keras
+from tensorflow.keras.layers import Conv2D as Conv
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.layers import MaxPooling2D as Pool
+
 sys.path.append("../DeepTrack")
 
-from DeepTrack.Generators import Generator
-from DeepTrack.Particles import PointParticle
+from DeepTrack.Augmentation import FlipLR, FlipUD, NormalizeMinMax, Transpose
 from DeepTrack.Backend.Distributions import uniform_random
-from DeepTrack.Noise import Gaussian, Offset
-from DeepTrack.Optics import BaseOpticalDevice2D
 from DeepTrack.Backend.Image import Image
 from DeepTrack.Callbacks import Storage
-from DeepTrack.Augmentation import FlipLR, FlipUD, NormalizeMinMax
+from DeepTrack.Generators import Generator
+from DeepTrack.Models import DeepTrackNetwork
+from DeepTrack.Noise import Gaussian, Offset
+from DeepTrack.Optics import BaseOpticalDevice2D
+from DeepTrack.Particles import PointParticle
 
-from tensorflow import keras
-from tensorflow.keras.layers import Conv2D as Conv, MaxPooling2D as Pool, Dense, Flatten
-import numpy as np
-import matplotlib.pyplot as plt
-from timeit import default_timer as timer
+
+
+
 
 '''
     Simple example showcasing current possiblites.
     
-    A generator is created with certain optical properties. A simple tracker is trained.
+    A generator is created with certain optical properties. A simple tracker is trained on particles with added gaussian noise.
  '''
 
 
@@ -40,42 +48,33 @@ P = PointParticle(                                         # Radius of the gener
     position_distribution=uniform_random((64,64,20))           # The distrbution from which to draw the position of the particle
 )
 
+N = Gaussian(
+    sigma=np.linspace(0.03,0.08),
+)
 
 # Create a model. This model is likely too simple to achieve subpixel resolution.
-model = keras.models.Sequential()
-model.add(Conv(16, kernel_size=3, activation="relu", padding="same", input_shape=(64,64,1)))
-model.add(Pool(2))
-model.add(Conv(32, kernel_size=3, activation="relu", padding="same"))
-model.add(Pool(2))
-model.add(Conv(64, kernel_size=3, activation="relu", padding="same"))
-model.add(Conv(2, kernel_size=3, activation="relu", padding="same"))
-model.add(Flatten())
-model.add(Dense(64))
-model.add(Dense(2))
-model.compile(keras.optimizers.Adam(), loss="mae")
+model = DeepTrackNetwork(input_shape=(64,64,1), number_of_outputs=2)
+model.compile(keras.optimizers.Adam(), loss="rmse")
 
 
 # Create your generators. (Features to generate, Labels to extract, augmentations, batch_size)
-training_generator = G.generate(P, ["x", "y"], augmentation=[NormalizeMinMax(), FlipLR(), FlipUD()], batch_size=100)
-validation_generator = G.generate(P, ["x", "y"], augmentation=[NormalizeMinMax(), FlipLR(), FlipUD()], batch_size=4)
+training_generator =   G.generate(P + N, ["x", "y"], augmentation=[NormalizeMinMax(), FlipLR(), FlipUD(), Transpose()], batch_size=100)
+validation_generator = G.generate(P + N, ["x", "y"], augmentation=[NormalizeMinMax(), FlipLR(), FlipUD(), Transpose()], batch_size=10)
 
 model.fit_generator(training_generator,  
-                        steps_per_epoch=4,
-                        epochs=100, 
-                        validation_data=validation_generator,
-                        validation_steps=1,
-                        workers=1)
+                        steps_per_epoch=32,
+                        epochs=100,
+                        workers=1,
+                        use_multiprocessing=False)
 
 test_batch, labels = next(validation_generator)
 test_prediction = model.predict(test_batch)
 
 
 plt.gray()
-for i in range(4):
-    plt.subplot(221 + i)
+for i in range(9):
+    plt.subplot(331 + i)
     plt.imshow(np.squeeze(test_batch[i,:,:,0]))
     plt.scatter(labels[i,0], labels[i,1], 20, 'g')
     plt.scatter(test_prediction[i,0], test_prediction[i,1], 20, 'b')
 plt.show()
-
-
