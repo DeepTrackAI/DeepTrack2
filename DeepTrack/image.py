25b00ef152e5079e69f2a6732e4ffcdcf10f57e9
Benjamin Midtvedt <benmid@student.chalmers.se>
2019-10-29 15:16:47 +0100

    Documented image.py

--- DeepTrack/image.py
+++ DeepTrack/image.py
@@ -1,30 +1,77 @@
 from abc import ABC, abstractmethod
 import numpy as np
 import copy
-'''
-Make a subclass of ndarray
-'''
+
 class Image(np.ndarray):
+    '''Subclass of numpy ndarray with additional attribute "properties"
+
+    The Image subclass of numpy ndarrays are used by features to resolve 
+    images and store the current value of the properties of each feature 
+    in the feature series. These properties are stored in the field
+    `properties` as a list of dictionaries, in the same order that the 
+    features were evaluated. 
+
+    This is used thoughout the DeepTrack model to store and extract 
+    information about how an image was generated. Examples include
+
+    * Features may depend on earlier features in the feature series.
+
+    * The class `Label`, which extract numeric information 
+    about the image to be sent to machine learning models.   
+
+    * Loading previously generated images from storage without
+    losing information about the image.
+
+    Parameters
+    ----------
+    input_array : array_like
+        An array_like object that is used to instantiate the ndarray
+    properties : {None, list}
+        Optional parameter to set as the initial value for the field properties 
+
+    Attributes
+    ----------
+    properties : list
+        List of dictionaries of the current value of all properties of 
+        the features used to resolve the image.
+
+    '''
     
-    __array_priority__ = 2
-    
+    # Used by numpy to determine output type of u_funcs.
+    # This ensures that the output will always be an Image
+    __array_priority__ = 999
+
+
+    def append(self, property_dict:dict):
+        ''' Appends a dictionary to the properties list
+
+        Parameters
+        ----------
+        property_dict : dict
+            A dictionary to append to the property list. Most commonly 
+            the current values of the properties of a feature.
+        
+        Returns
+        -------
+        Image
+            Returns itself
+        '''
+        self.properties.append(property_dict)
+        return self
+
+
     def __new__(cls, input_array, properties=None):
+        # Converts input to ndarray, and then to an Image
         obj = np.asarray(input_array).view(cls)
         if properties is None:
             properties = []
         obj.properties = properties
         return obj
 
-
-    def append(self, properties):
-        self.properties.append(properties)
-
-    
     
     def __array_wrap__(self, out_arr, context=None):
         
         if out_arr is self:  # for in-place operations
-            
             result = out_arr
         else:
             result = Image(out_arr)
