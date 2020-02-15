c9ff7ab420208709e4a12fe7b1f32d05725d0cc3
BenjaminMidtvedt <41636530+BenjaminMidtvedt@users.noreply.github.com>
2020-02-15 19:10:07 +0100

    Added (3d) ellipsoidal scatterer. (#79)
    * added ellipsoid
    
    * documented ellipse

--- deeptrack/scatterers.py
+++ deeptrack/scatterers.py
@@ -134,20 +134,15 @@ class Ellipse(Scatterer):
 
 
 class Sphere(Scatterer):
-    ''' Generates ellipsoidal scatterers
+    ''' Generates spherical scatterers
 
     Parameters
     ----------
     position               
         The position of the point particle. Defined as (0,0) in the
         upper left corner.
-    intensity               
-        The magnitude of the complex field scattered by the point particle. 
-        Mathematically the integral over the delta distribution. 
     radius
         The radius of the sphere, in meters
-    rotation
-        If defined, rotates the ellipsoid by this amount in radians
     '''
 
     def get(
@@ -182,6 +177,110 @@ class Sphere(Scatterer):
                                  mask.shape[1] // upsample, upsample,
                                  mask.shape[2] // upsample, upsample)).mean(axis=(5, 3, 1))
 
+        mask = mask[~np.all(mask == 0, axis=(1, 2))]
+        mask = mask[:, ~np.all(mask == 0, axis=(0, 2))]
+        mask = mask[:, :, ~np.all(mask == 0, axis=(0, 1))]
+
+        return mask
+
+
+class Ellipsoid(Scatterer):
+    ''' Generates ellipsoidal scatterer
+
+    Parameters
+    ----------
+    position               
+        The position of the point particle. Defined as (0,0) in the
+        upper left corner.
+    radius
+        The radius of the ellipsoid along the principal axes in meters. Can be a single value to a vector 
+        of length 2 or 3.
+    rotation
+        The rotation about the three axes in radians. Can be a single value to a vector of length 2 or 3.
+    upsample
+        During the calculation of the scatterer, the pixelation is increased by this factor. The result is 
+        then downsampled by averaging.
+    '''
+    
+    def _process_properties(self, propertydict):
+        '''Preprocess the input to the method .get()
+
+        Ensures that the radius and the rotation properties both are arrays of
+        length 3.
+
+        If the radius is a single value, the particle is made a sphere
+        If the radius are two values, the smallest value is appended as the third value
+
+        The rotation vector is padded with zeros until it is of length 3
+
+        '''
+
+
+        # Ensure radius has three values
+        radius = propertydict["radius"]
+        if radius.size == 1:
+            # If only one value, assume sphere
+            radius = (*radius,) * 3
+        elif radius.size == 2:
+            # If two values, duplicate the minor axis
+            radius = (*radius, np.min(radius[-1]))
+        elif radius.size == 3:
+            # If three values, convert to tuple for consistency
+            radius = (*radius, )
+        propertydict["radius"] = radius
+
+        # Ensure rotation has three values
+        rotation = propertydict["rotation"]
+        if rotation.size == 1:
+            # If only one value, pad with two zeros
+            rotation = (*rotation, 0, 0) 
+        elif rotation.size == 2:
+            # If two values, pad with one zero
+            rotation = (*rotation, 0)
+        elif rotation.size == 3:
+            # If three values, convert to tuple for consistency
+            rotation = (*rotation, )
+
+
+    def get(self,
+            image,
+            radius=None,
+            rotation=None,
+            voxel_size=None,
+            upsample=4,
+            **kwargs):
+
+
+        radius_in_pixels = radius / voxel_size * upsample
+        max_rad = np.max(radius) / voxel_size * upsample
+        rad_ceil = np.ceil(max_rad)
+        to_add = (upsample - ((rad_ceil * 2) % upsample)) % upsample
+
+        # Create a rotated grid of points
+        x = np.arange(-rad_ceil[0], rad_ceil[0] + to_add[0])
+        y = np.arange(-rad_ceil[1], rad_ceil[1] + to_add[1])
+        z = np.arange(-rad_ceil[2], rad_ceil[2] + to_add[2])
+
+        X, Y, Z = np.meshgrid(x, y, z)
+
+        mask = X + Y + Z < 1
+        cos = np.cos(rotation)
+        sin = np.sin(rotation)
+
+        XR = cos[0] * cos[1] * X + (cos[0] * sin[1] * sin[2] - sin[0] * cos[2]) * Y + (cos[0] * sin[1] * cos[2] + sin[0] * sin[2]) * Z
+        YR = sin[0] * cos[1] * X + (sin[0] * sin[1] * sin[2] + cos[0] * cos[2]) * Y + (sin[0] * sin[1] * cos[2] - cos[0] * sin[2]) * Z
+        ZR = -sin[1] * X + cos[1] * sin[2] * Y + cos[1] * cos[2] * Z 
+
+        mask = (XR / radius_in_pixels[0])**2 + (YR / radius_in_pixels[1])**2 + (ZR / radius_in_pixels[2])**2 < 1
+
+        # Downsample
+        if upsample != 1:
+            mask = np.reshape(mask, 
+                                (mask.shape[0] // upsample, upsample, 
+                                 mask.shape[1] // upsample, upsample,
+                                 mask.shape[2] // upsample, upsample)).mean(axis=(5, 3, 1))
+
+        # Crop all rows/columns that contain no non-zero entry
         mask = mask[~np.all(mask == 0, axis=(1, 2))]
         mask = mask[:, ~np.all(mask == 0, axis=(0, 2))]
         mask = mask[:, :, ~np.all(mask == 0, axis=(0, 1))]
