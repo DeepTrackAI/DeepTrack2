a9364583eb081033c41f0617da239dc8a4fb34bc
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2020-03-08 19:39:58 +0100

    Create test_image.py

---
+++ test/test_image.py
@@ -0,0 +1,15 @@
+import unittest
+
+import deeptrack.image
+
+from deeptrack.features import Feature
+
+
+
+class TestUtils(unittest.TestCase):
+    pass
+
+    
+    
+if __name__ == '__main__':
+    unittest.main()
