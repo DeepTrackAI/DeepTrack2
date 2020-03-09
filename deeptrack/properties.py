12ec39af9b790dd905515b137469dc160606efb9
BenjaminMidtvedt <41636530+BenjaminMidtvedt@users.noreply.github.com>
2020-03-09 15:11:38 +0100

    Bm document properties (#117)
    * documented properties
    
    * documented properties

--- deeptrack/properties.py
+++ deeptrack/properties.py
@@ -1,21 +1,25 @@
 '''Tools to manage the properties of features
 
-This module contains:
-
+CLASSES
+-------
+Property
 The class `Property`, which represents the values of a property of a feature.
 A Property can be represented by:
-* A constant (inizialization with, e.g., a number, a tuple)
-* A sequence of variables (inizialization with, e.g., a generator)
-* A discrete random variable (inizialization with, e.g., a list, a dictionary)
-* A continuous random variable (inizialization with, e.g., a function)
+* A constant (initialization with, e.g., a number, a tuple)
+* A sequence of variables (initialization with, e.g., an iterator)
+* A random variable (initialization with, e.g., a function)
 
+SequentialProperty
+The class `SequentialProperty` which extends `Property` to sample one value
+for each step in a sequence.
+
+PropertyDict
 The class `PropertyDict`, which is a dictionary with each element a Property.
 The class provides utility functions to update, sample, clear and retrieve
 properties.
 
 '''
 import numpy as np
-import inspect
 from deeptrack.utils import isiterable, hasmethod, get_kwarg_names
 
 
@@ -23,7 +27,7 @@ from deeptrack.utils import isiterable, hasmethod, get_kwarg_names
 # CLASSES
 
 class Property:
-    r'''Represents a property of a feature
+    '''Represents a property of a feature
 
     The class Property wraps an input, which is treated
     internally as a sampling rule. This sampling rule is used
@@ -46,31 +50,8 @@ class Property:
         The sampling rule to update the value of the feature property.
     current_value : any
         The current value obtained from the last call to the sampling rule.
-
-    Examples
-    --------
-    When the sampling rule is a number,
-    the current value is always the number itself:
-
-    >>> D = Property(1)
-    >>> D.current_value
-    1
-    >>> D.update()
-    >>> D.current_value
-    1
-
-    When the sampling rule is a list,
-    the current value is one of the elements of the list:
-
-    >>> np.random.seed(0)
-    >>> D = Property([1, 2, 3])
-    >>> D.current_value # Either 1, 2 or 3 randomly
-    2 #random
-    >>> D.current_value # Same as last call
-    2 #random
-    >>> D.update()
-    >>> D.current_value
-    1 #random
+    has_updated_since_last_resolve : bool
+        Whether the property has been updated since the last resolve.
 
     '''
 
@@ -81,7 +62,7 @@ class Property:
 
     @property
     def current_value(self):
-        r'''Current value of the property of the feature
+        '''Current value of the property of the feature
 
         `current_value` is the result of the latest `update()` call.
         Note that any randomization only occurs when the method `update()` is called
@@ -105,11 +86,19 @@ class Property:
         return self._current_value
 
 
-    def update(self, **kwargs) -> 'Property':
+    def update(self, force_update: bool = False, **kwargs) -> 'Property':
         r'''Updates the current value
 
         The method `update()` sets the property `current_value`
-        as the output of the method `sample()`.
+        as the output of the method `sample()`. Will only update
+        once per resolve.
+
+        Any object that implements the method `update()` will have it called.
+
+        Parameters
+        ----------
+        force_update
+            If True, ignore the one update per resolve limit.
 
         Returns
         -------
@@ -118,21 +107,21 @@ class Property:
 
         '''
 
-        if not kwargs.get("force_update", False) and self.has_updated_since_last_resolve:
+        if not force_update and self.has_updated_since_last_resolve:
             return self
-        
-        self.has_updated_since_last_resolve = True
 
         if hasmethod(self.sampling_rule, "update"):
-            self.sampling_rule.update(**kwargs)
+            self.sampling_rule.update(force_update=force_update, **kwargs)
+        
+        self.current_value = self.sample(self.sampling_rule, force_update=force_update, **kwargs)
 
-        self.current_value = self.sample(self.sampling_rule, **kwargs)
+        self.has_updated_since_last_resolve = True
 
         return self
 
 
     def sample(self, sampling_rule, **kwargs):
-        r'''Samples the sampling rule
+        '''Samples the sampling rule
 
         Returns a sampled instance of the `sampling_rule` field.
         The logic behind the sampling depends on the type of
@@ -141,15 +130,21 @@ class Property:
 
         1. Any object with a callable `sample()` method has this
             method called and returned.
-        2. If the rule is a ``dict``, the ``dict`` is copied and any value
-            with has a callable sample method is replaced with the
-            output of that call.
-        3. If the rule is a ``list`` or a 1-dimensional ``ndarray``, return
+        2. If the rule is a ``dict``, sample each value and combine the 
+            result into a new ``dict`` using the original keys.
+        3. If the rule is a ``list``, sample each element of the list and
+            combine the result into a ne ``list``.
         4. If the rule is an ``iterable``, return the next output.
-        5. If the rule is callable, return the output of a call with
-            no input parameters.
-            a single element drawn from that ``list``/``ndarray``.
+        5. If the rule is callable, call it with its accepted arguments.
+            Example arguments can be the value of some other property.
         6. If none of the above apply, return the rule itself.
+        
+        Parameters
+        ----------
+        sampling_rule : any
+            The rule to sample values from.
+        **kwargs
+            Arguments that will be passed on to functions that accepts them.
 
         Returns
         -------
@@ -168,7 +163,7 @@ class Property:
             # element being sampled from the original dict.
             out = {}
             for key, val in self.sampling_rule.items():
-                    out[key] = self.sample(val, **kwargs)
+                out[key] = self.sample(val, **kwargs)
             return out
 
         elif isinstance(sampling_rule, list):
@@ -186,11 +181,15 @@ class Property:
                 return self.current_value
   
         elif callable(sampling_rule):
-            # If it's a function
+            # If it's a function, extract the arguments it accepts.
             function_input = {}
+
+            # Get the kwarg arguments the function accepts
             for key in get_kwarg_names(sampling_rule):
+                # If that name is among passed kwarg arguments
                 if key in kwargs:
                     if isinstance(kwargs[key], Property):
+                        # If it is a property, update it and pass the current value
                         kwargs[key].update(**kwargs)
                         
                         if isinstance(kwargs[key], SequentialProperty):
@@ -198,8 +197,6 @@ class Property:
                         else:
                             kwargs[key] = kwargs[key].current_value
 
-                    
-                         
                     function_input[key] = kwargs[key]
 
             return sampling_rule(**function_input)
@@ -210,6 +207,28 @@ class Property:
 
 
 class SequentialProperty(Property):
+    ''' Property that has multiple sequential values
+
+        Extends standard Property to resolve one value for each step
+        in the sequence.
+
+        Parameters
+        ----------
+        initializer : any
+            Sampling rule for the first step of the sequence
+        sampling_rule : any
+            Sampling rule for each step after the first
+        
+        Attributes
+        ----------
+        initializer : any
+            Sampling rule for the first step of the sequence
+        sampling_rule : any
+            Sampling rule for each step after the first
+        has_updated_since_last_resolve : bool
+            Whether the property has been updated since the last resolve.
+
+    '''
 
     def __init__(self, initializer, sampling_rule):
         self.initializer = initializer
@@ -218,6 +237,31 @@ class SequentialProperty(Property):
         
 
     def update(self, sequence_length=0, **kwargs):
+        '''Updates current_value
+
+        For each step in the sequence, sample `self.sampling_rule`.
+        `self.initializer` is used for the first step. These rules
+        should output one value per step. Sampling rules
+        that are functions can optionally accept the following keyword
+        arguments:
+        
+        * sequence_step : the current position in the sequence.
+        * sequence_length : the length of the sequence.
+        * previous_value : the value of the property at the previous
+          step in the sequence.
+        * previous_values : the value of the property at all previous
+          steps in the sequence.
+
+        Parameters
+        ----------
+        sequence_length : int, optional
+            length of the sequence
+        
+        Returns
+        -------
+        self
+            returns self
+        '''
 
         if self.has_updated_since_last_resolve:
             return self
@@ -227,8 +271,10 @@ class SequentialProperty(Property):
         new_current_value = []
 
         for step in range(sequence_length):
-
+            # Use initializer for first time step
             ruleset = self.initializer if step == 0 else self.sampling_rule
+
+            # Elements inserted here can be passed to property functions
             kwargs.update(
                 sequence_step=step,
                 sequence_length=sequence_length,
@@ -299,6 +345,7 @@ class PropertyDict(dict):
         for property in self.values():
             kwargs.update(self)
             property.update(**kwargs)
+
         return self
 
 
