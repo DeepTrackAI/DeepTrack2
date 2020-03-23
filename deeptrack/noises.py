2d8c7da4dadff212bad01a22fd3f53b036509261
Benjamin Midtvedt <benmid@student.chalmers.se>
2020-03-23 16:11:16 +0100

    document noise

--- deeptrack/noises.py
+++ deeptrack/noises.py
@@ -6,25 +6,30 @@ from deeptrack.image import Image
 
 
 class Noise(Feature):
-    '''Base class for the Noise object.
-
-    Creates an image of desired shape, as defined by the implementing class.
-
-    Basic operators are overloaded to easily allow it to be added to an image
-    without explicity generating a new image each time
+    '''Base class for the Noise object. 
     '''
 
 
 
-class Offset(Noise):
-    def get(self, image, offset=0, **kwargs):
+class Add(Noise):
+    ''' Adds a constant value to an image
+    Parameters
+    ----------
+    offset : float
+        The value to add to the image
+    '''
+    def __init__(self, offset, **kwargs):
+        super().__init__(offset=offset, **kwargs)
+
+    def get(self, image, offset, **kwargs):
         return image + offset
+# ALIASES
+Offset = Add
+Background = Add
 
 
-    
 class Gaussian(Noise):
-    '''Adds gaussian noise to image
-    Implementation of the Noise class to generate IID gaussian pixels.
+    '''Adds IID Gaussian noise to an image
 
     Parameters
     ----------
@@ -33,10 +38,10 @@ class Gaussian(Noise):
     sigma
         The root of the variance of the distribution.
     '''
-    def __init__(self, *args, mu=0, sigma=1, **kwargs):
+    def __init__(self, *args, mu, sigma, **kwargs):
         super().__init__(*args, mu=mu, sigma=sigma, **kwargs)
 
-    def get(self, image, mu=0, sigma=1, **kwargs):
+    def get(self, image, mu, sigma, **kwargs):
         mu = np.ones(image.shape) * mu
         sigma = np.ones(image.shape) * sigma
         noisy_image = image + np.random.normal(mu, sigma)
@@ -45,6 +50,14 @@ class Gaussian(Noise):
 
 
 class Poisson(Noise):
+     '''Adds Poisson-distributed noise to an image
+
+    Parameters
+    ----------
+    snr
+        Signal to noise ratio of the final image. The signal is determined
+        by the peak value of the image.
+    '''
     def get(self, image, snr=None, **kwargs):
         image[image < 0] = 0
         peak = np.max(image)
