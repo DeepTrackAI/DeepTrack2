2d1a0cbf473dc9cfd66abcd43a33f8b07ed44027
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2020-03-26 10:07:24 +0100

    Update test_optics.py

--- test/test_optics.py
+++ test/test_optics.py
@@ -58,7 +58,30 @@ class TestOptics(unittest.TestCase):
         self.assertEqual(output_image.shape, (64, 64, 1))        
 
 
-    #TODO IlluminationGradient
+    def test_IlluminationGradient(self):
+        illumination_gradient = optics.IlluminationGradient(gradient=(5e-5, 5e-5))
+        microscope = optics.Brightfield(
+            NA=0.7,
+            wavelength=660e-9,
+            resolution=1e-6,
+            magnification=10,
+            refractive_index_medium=1.33,
+            upscale=2,
+            output_region=(0, 0, 64, 64),
+            padding=(10, 10, 10, 10),
+            aberration=None,
+            illumination=illumination_gradient
+        )
+        scatterer = PointParticle(
+            refractive_index=1.45 + 0.1j,
+            position_unit="pixel",
+            position=(32, 32),
+        )
+        imaged_scatterer = microscope(scatterer)
+        output_image = imaged_scatterer.resolve()
+        self.assertIsInstance(output_image, Image)        
+        self.assertEqual(output_image.shape, (64, 64, 1))        
+
 
 
 if __name__ == '__main__':
