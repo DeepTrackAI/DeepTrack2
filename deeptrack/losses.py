4d5761ca4dd9150366da6551531dadd60d7cbfc0
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2020-04-07 09:12:50 +0200

    Update losses.py

--- deeptrack/losses.py
+++ deeptrack/losses.py
@@ -21,16 +21,17 @@ nd_binary_crossentropy
 nd_kullback_leibler_divergence
     Kullback-Leibler divergence error with flattened inputs.
 nd_mean_absolute_error
-    Mean absolute error with flattened inputs
+    Mean absolute error with flattened inputs.
 nd_mean_absolute_percentage_error
-    Mean absolute percentage error with flattened inputs
-
+    Mean absolute percentage error with flattened inputs.
 '''
 
 from tensorflow import keras
 
 losses = keras.losses
 K = keras.backend
+
+
 
 # Extends the following loss functions
 _COMPATIBLE_LOSS_FUNCTIONS = [
@@ -43,6 +44,8 @@ _COMPATIBLE_LOSS_FUNCTIONS = [
     losses.mae,
     losses.mape
 ]
+
+
 
 # LOSS WRAPPERS
 def flatten(func):
@@ -64,6 +67,7 @@ def flatten(func):
         return func(T, P)
     wrapper.__name__ = "nd_" + func.__name__
     return wrapper
+
 
 
 def sigmoid(func):
@@ -88,6 +92,7 @@ def sigmoid(func):
     return wrapper
 
 
+
 def weighted_crossentropy(weight=(1, 1), eps=1e-4):
     '''Binary crossentropy with weighted classes.
     
@@ -104,6 +109,7 @@ def weighted_crossentropy(weight=(1, 1), eps=1e-4):
     def unet_crossentropy(T, P):
         return -K.mean(weight[0] * T * K.log(P + eps) + weight[1] * (1 - T) * K.log(1 - P + eps)) / (weight[0] + weight[1])
     return unet_crossentropy
+
 
 
 # Wrap standard keras loss function with flatten.
