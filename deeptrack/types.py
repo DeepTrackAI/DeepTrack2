3463a6786ac40efcf041a78288d2b2e649594c47
Benjamin Midtvedt <benmid@student.chalmers.se>
2021-02-25 17:21:09 +0100

    Added arraylike type

--- deeptrack/types.py
+++ deeptrack/types.py
@@ -2,7 +2,10 @@
 """
 
 import typing
+import numpy as np
 
 # Property type declaration
 T = typing.TypeVar("T")
 PropertyLike = typing.Union[T, typing.Callable[..., T]]
+
+ArrayLike = typing.Union[typing.Tuple[T], typing.List[T], np.ndarray]
