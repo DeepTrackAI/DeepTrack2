05610cfbb0f1d99bf331f2a75751341de87e59ca
Benjamin Midtvedt <benmid@student.chalmers.se>
2021-04-26 23:13:49 +0200

    Allow operators between nodes

--- deeptrack/backend/core.py
+++ deeptrack/backend/core.py
@@ -1,4 +1,5 @@
 import numpy as np
+import operator
 
 
 class DeepTrackDataObject:
@@ -83,7 +84,7 @@ class DeepTrackNode:
             self.store(value)
 
     def update(self):
-        self.data.invalidate()
+        self.invalidate()
         for dependency in self.dependencies:
             dependency.update()
 
@@ -99,3 +100,74 @@ class DeepTrackNode:
 
     def previous(self):
         return self.data.current_value()
+
+    def __add__(self, other):
+        return create_node_with_operator(operator.__add__, self, other)
+
+    def __radd__(self, other):
+        return create_node_with_operator(operator.__add__, other, self)
+
+    def __sub__(self, other):
+        return create_node_with_operator(operator.__sub__, self, other)
+
+    def __rsub__(self, other):
+        return create_node_with_operator(operator.__sub__, other, self)
+
+    def __mul__(self, other):
+        return create_node_with_operator(operator.__mul__, self, other)
+
+    def __rmul__(self, other):
+        return create_node_with_operator(operator.__mul__, other, self)
+
+    def __truediv__(self, other):
+        return create_node_with_operator(operator.__truediv__, self, other)
+
+    def __rtruediv__(self, other):
+        return create_node_with_operator(operator.__truediv__, other, self)
+
+    def __floordiv__(self, other):
+        return create_node_with_operator(operator.__floordiv__, self, other)
+
+    def __rfloordiv__(self, other):
+        return create_node_with_operator(operator.__floordiv__, other, self)
+
+    def __lt__(self, other):
+        return create_node_with_operator(operator.__lt__, self, other)
+
+    def __rlt__(self, other):
+        return create_node_with_operator(operator.__lt__, other, self)
+
+    def __gt__(self, other):
+        return create_node_with_operator(operator.__gt__, self, other)
+
+    def __rgt__(self, other):
+        return create_node_with_operator(operator.__gt__, other, self)
+
+    def __le__(self, other):
+        return create_node_with_operator(operator.__le__, self, other)
+
+    def __rle__(self, other):
+        return create_node_with_operator(operator.__le__, other, self)
+
+    def __ge__(self, other):
+        return create_node_with_operator(operator.__ge__, self, other)
+
+    def __rge__(self, other):
+        return create_node_with_operator(operator.__ge__, other, self)
+
+
+def create_node_with_operator(op, a, b):
+
+    if not issubclass(a, DeepTrackNode):
+        a = DeepTrackNode(a)
+
+    if not issubclass(b, DeepTrackNode):
+        b = DeepTrackNode(b)
+
+    new = DeepTrackNode(lambda: op(a(), b()))
+    new.add_dependency(a)
+    new.add_dependency(b)
+    a.add_child(new)
+    b.add_child(new)
+
+    return new
