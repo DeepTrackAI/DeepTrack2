c16fa6fee793b099b8de89378c86ef26a356077c
Benjamin Midtvedt <benmid@student.chalmers.se>
2021-04-29 23:45:10 +0200

    Reintroduce old tests

--- deeptrack/test/test_models.py
+++ deeptrack/test/test_models.py
@@ -1,97 +1,97 @@
-# import sys
+import sys
 
-# # sys.path.append(".")  # Adds the module to path
+# sys.path.append(".")  # Adds the module to path
 
-# import unittest
+import unittest
 
-# from .. import models
-# import numpy as np
-
-
-# class TestModels(unittest.TestCase):
-#     def test_FullyConnected(self):
-#         model = models.FullyConnected(
-#             input_shape=(64, 2),
-#             dense_layers_dimensions=(32, 32),
-#             number_of_outputs=3,
-#             output_activation="sigmoid",
-#             loss="mse",
-#             flatten_input=True,
-#         )
-#         self.assertIsInstance(model, models.KerasModel)
-
-#         model.predict(np.zeros((1, 64, 2)))
-
-#     def test_Convolutions_with_single_input(self):
-#         model = models.Convolutional(
-#             input_shape=(64, 64, 1),
-#             conv_layers_dimensions=(16, 32, 64),
-#             dense_layers_dimensions=(32, 32),
-#             number_of_outputs=3,
-#             output_activation="sigmoid",
-#             loss="mse",
-#         )
-#         self.assertIsInstance(model, models.KerasModel)
-
-#         model.predict(np.zeros((1, 64, 64, 1)))
-
-#     def test_Convolutions_with_multiple_inputs(self):
-#         model = models.Convolutional(
-#             input_shape=[(64, 64, 1), (64, 64, 2)],
-#             conv_layers_dimensions=(16, 32, 64),
-#             dense_layers_dimensions=(32, 32),
-#             number_of_outputs=3,
-#             output_activation="sigmoid",
-#             loss="mse",
-#         )
-#         self.assertIsInstance(model, models.KerasModel)
-
-#         model.predict([np.zeros((1, 64, 64, 1)), np.zeros((1, 64, 64, 2))])
-
-#     def test_Convolutions_with_no_dense_top(self):
-#         model = models.Convolutional(
-#             input_shape=(64, 64, 1),
-#             conv_layers_dimensions=(16, 32, 64),
-#             dense_layers_dimensions=(32, 32),
-#             dense_top=False,
-#             number_of_outputs=3,
-#             output_activation="sigmoid",
-#             loss="mse",
-#         )
-#         self.assertIsInstance(model, models.KerasModel)
-
-#         model.predict(np.zeros((1, 64, 64, 1)))
-
-#     def test_UNet(self):
-#         model = models.UNet(
-#             input_shape=(64, 64, 1),
-#             conv_layers_dimensions=(16, 32, 64),
-#             base_conv_layers_dimensions=(256, 256),
-#             output_conv_layers_dimensions=(32, 32),
-#             steps_per_pooling=1,
-#             number_of_outputs=1,
-#             output_activation="sigmoid",
-#             loss="mse",
-#         )
-#         self.assertIsInstance(model, models.KerasModel)
-
-#         model.predict(np.zeros((1, 64, 64, 1)))
-
-#     def test_RNN(self):
-#         model = models.rnn(
-#             input_shape=(None, 64, 64, 1),
-#             conv_layers_dimensions=(16, 32, 64),
-#             dense_layers_dimensions=(32,),
-#             rnn_layers_dimensions=(32,),
-#             steps_per_pooling=1,
-#             number_of_outputs=1,
-#             output_activation="sigmoid",
-#             loss="mse",
-#         )
-#         self.assertIsInstance(model, models.KerasModel)
-
-#         model.predict(np.zeros((1, 1, 64, 64, 1)))
+from .. import models
+import numpy as np
 
 
-# if __name__ == "__main__":
-#     unittest.main()
+class TestModels(unittest.TestCase):
+    def test_FullyConnected(self):
+        model = models.FullyConnected(
+            input_shape=(64, 2),
+            dense_layers_dimensions=(32, 32),
+            number_of_outputs=3,
+            output_activation="sigmoid",
+            loss="mse",
+            flatten_input=True,
+        )
+        self.assertIsInstance(model, models.KerasModel)
+
+        model.predict(np.zeros((1, 64, 2)))
+
+    def test_Convolutions_with_single_input(self):
+        model = models.Convolutional(
+            input_shape=(64, 64, 1),
+            conv_layers_dimensions=(16, 32, 64),
+            dense_layers_dimensions=(32, 32),
+            number_of_outputs=3,
+            output_activation="sigmoid",
+            loss="mse",
+        )
+        self.assertIsInstance(model, models.KerasModel)
+
+        model.predict(np.zeros((1, 64, 64, 1)))
+
+    def test_Convolutions_with_multiple_inputs(self):
+        model = models.Convolutional(
+            input_shape=[(64, 64, 1), (64, 64, 2)],
+            conv_layers_dimensions=(16, 32, 64),
+            dense_layers_dimensions=(32, 32),
+            number_of_outputs=3,
+            output_activation="sigmoid",
+            loss="mse",
+        )
+        self.assertIsInstance(model, models.KerasModel)
+
+        model.predict([np.zeros((1, 64, 64, 1)), np.zeros((1, 64, 64, 2))])
+
+    def test_Convolutions_with_no_dense_top(self):
+        model = models.Convolutional(
+            input_shape=(64, 64, 1),
+            conv_layers_dimensions=(16, 32, 64),
+            dense_layers_dimensions=(32, 32),
+            dense_top=False,
+            number_of_outputs=3,
+            output_activation="sigmoid",
+            loss="mse",
+        )
+        self.assertIsInstance(model, models.KerasModel)
+
+        model.predict(np.zeros((1, 64, 64, 1)))
+
+    def test_UNet(self):
+        model = models.UNet(
+            input_shape=(64, 64, 1),
+            conv_layers_dimensions=(16, 32, 64),
+            base_conv_layers_dimensions=(256, 256),
+            output_conv_layers_dimensions=(32, 32),
+            steps_per_pooling=1,
+            number_of_outputs=1,
+            output_activation="sigmoid",
+            loss="mse",
+        )
+        self.assertIsInstance(model, models.KerasModel)
+
+        model.predict(np.zeros((1, 64, 64, 1)))
+
+    def test_RNN(self):
+        model = models.rnn(
+            input_shape=(None, 64, 64, 1),
+            conv_layers_dimensions=(16, 32, 64),
+            dense_layers_dimensions=(32,),
+            rnn_layers_dimensions=(32,),
+            steps_per_pooling=1,
+            number_of_outputs=1,
+            output_activation="sigmoid",
+            loss="mse",
+        )
+        self.assertIsInstance(model, models.KerasModel)
+
+        model.predict(np.zeros((1, 1, 64, 64, 1)))
+
+
+if __name__ == "__main__":
+    unittest.main()
--- deeptrack/test/test_utils.py
+++ deeptrack/test/test_utils.py
@@ -1,111 +1,111 @@
-# import sys
+import sys
 
-# # sys.path.append(".")  # Adds the module to path
+# sys.path.append(".")  # Adds the module to path
 
-# import unittest
+import unittest
 
-# from .. import utils
-
-
-# class TestUtils(unittest.TestCase):
-#     def test_hasmethod(self):
-#         self.assertTrue(utils.hasmethod(utils, "hasmethod"))
-#         self.assertFalse(
-#             utils.hasmethod(utils, "this_is_definetely_not_a_method_of_utils")
-#         )
-
-#     def test_isiterable(self):
-#         self.assertFalse(utils.isiterable(1))
-
-#         non_iterable_obj = ("apple", "banana", "cherry")
-#         self.assertFalse(utils.isiterable(non_iterable_obj))
-
-#         iterable_obj = iter(("apple", "banana", "cherry"))
-#         self.assertTrue(utils.isiterable(iterable_obj))
-
-#     def test_as_list(self):
-
-#         obj = 1
-#         self.assertEqual(utils.as_list(obj), [obj])
-
-#         list_obj = [1, 2, 3]
-#         self.assertEqual(utils.as_list(list_obj), list_obj)
-
-#     def test_get_kwarg_names(self):
-#         def func1():
-#             pass
-
-#         self.assertEqual(utils.get_kwarg_names(func1), [])
-
-#         def func2(key1):
-#             pass
-
-#         self.assertEqual(utils.get_kwarg_names(func2), ["key1"])
-
-#         def func3(key1, key2=2):
-#             pass
-
-#         self.assertEqual(utils.get_kwarg_names(func3), ["key1", "key2"])
-
-#         def func4(key1, *argv, key2=2):
-#             pass
-
-#         self.assertEqual(utils.get_kwarg_names(func4), ["key2"])
-
-#         def func5(*argv, key1, key2=2):
-#             pass
-
-#         self.assertEqual(utils.get_kwarg_names(func5), ["key1", "key2"])
-
-#         def func6(key1, key2, key3, *argv):
-#             pass
-
-#         self.assertEqual(utils.get_kwarg_names(func6), [])
-
-#         def func7(key1, key2=1, key3=3, **kwargs):
-#             pass
-
-#         self.assertEqual(utils.get_kwarg_names(func7), ["key1", "key2", "key3"])
-
-#     def test_safe_call(self):
-
-#         arguments = {
-#             "key1": None,
-#             "key2": False,
-#             "key_not_in_function": True,
-#             "key_not_in_function_2": True,
-#         }
-
-#         def func1():
-#             pass
-
-#         utils.safe_call(func1, **arguments)
-
-#         def func2(key1):
-#             pass
-
-#         utils.safe_call(func2, **arguments)
-
-#         def func3(key1, key2=2):
-#             pass
-
-#         utils.safe_call(func3, **arguments)
-
-#         def func4(key1, *argv, key2=2):
-#             pass
-
-#         self.assertRaises(TypeError, lambda: utils.safe_call(func4, **arguments))
-
-#         def func5(*argv, key1, key2=2):
-#             pass
-
-#         utils.safe_call(func5, **arguments)
-
-#         def func6(key1, key2=1, key3=3, **kwargs):
-#             pass
-
-#         utils.safe_call(func6, **arguments)
+from .. import utils
 
 
-# if __name__ == "__main__":
-#     unittest.main()
+class TestUtils(unittest.TestCase):
+    def test_hasmethod(self):
+        self.assertTrue(utils.hasmethod(utils, "hasmethod"))
+        self.assertFalse(
+            utils.hasmethod(utils, "this_is_definetely_not_a_method_of_utils")
+        )
+
+    def test_isiterable(self):
+        self.assertFalse(utils.isiterable(1))
+
+        non_iterable_obj = ("apple", "banana", "cherry")
+        self.assertFalse(utils.isiterable(non_iterable_obj))
+
+        iterable_obj = iter(("apple", "banana", "cherry"))
+        self.assertTrue(utils.isiterable(iterable_obj))
+
+    def test_as_list(self):
+
+        obj = 1
+        self.assertEqual(utils.as_list(obj), [obj])
+
+        list_obj = [1, 2, 3]
+        self.assertEqual(utils.as_list(list_obj), list_obj)
+
+    def test_get_kwarg_names(self):
+        def func1():
+            pass
+
+        self.assertEqual(utils.get_kwarg_names(func1), [])
+
+        def func2(key1):
+            pass
+
+        self.assertEqual(utils.get_kwarg_names(func2), ["key1"])
+
+        def func3(key1, key2=2):
+            pass
+
+        self.assertEqual(utils.get_kwarg_names(func3), ["key1", "key2"])
+
+        def func4(key1, *argv, key2=2):
+            pass
+
+        self.assertEqual(utils.get_kwarg_names(func4), ["key2"])
+
+        def func5(*argv, key1, key2=2):
+            pass
+
+        self.assertEqual(utils.get_kwarg_names(func5), ["key1", "key2"])
+
+        def func6(key1, key2, key3, *argv):
+            pass
+
+        self.assertEqual(utils.get_kwarg_names(func6), [])
+
+        def func7(key1, key2=1, key3=3, **kwargs):
+            pass
+
+        self.assertEqual(utils.get_kwarg_names(func7), ["key1", "key2", "key3"])
+
+    def test_safe_call(self):
+
+        arguments = {
+            "key1": None,
+            "key2": False,
+            "key_not_in_function": True,
+            "key_not_in_function_2": True,
+        }
+
+        def func1():
+            pass
+
+        utils.safe_call(func1, **arguments)
+
+        def func2(key1):
+            pass
+
+        utils.safe_call(func2, **arguments)
+
+        def func3(key1, key2=2):
+            pass
+
+        utils.safe_call(func3, **arguments)
+
+        def func4(key1, *argv, key2=2):
+            pass
+
+        self.assertRaises(TypeError, lambda: utils.safe_call(func4, **arguments))
+
+        def func5(*argv, key1, key2=2):
+            pass
+
+        utils.safe_call(func5, **arguments)
+
+        def func6(key1, key2=1, key3=3, **kwargs):
+            pass
+
+        utils.safe_call(func6, **arguments)
+
+
+if __name__ == "__main__":
+    unittest.main()
