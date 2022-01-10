4ef6ebc5baf9d26292372eebd9b8c508abab0a01
BenjaminMidtvedt <41636530+BenjaminMidtvedt@users.noreply.github.com>
2022-01-10 08:44:17 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import pkg_resources
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-required = ["tensorflow", "numpy", "scipy", "scikit-image"]
+required = ["tensorflow", "numpy", "scipy", "scikit-image", "pint"]
 installed = [pkg.key for pkg in pkg_resources.working_set]
 if (
     not "tensorflow" in installed
