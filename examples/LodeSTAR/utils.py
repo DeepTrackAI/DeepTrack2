c2fe94fb56bb57907951ee73481ce5ec69db5861
Benjamin Midtvedt <benmid@student.chalmers.se>
2022-05-29 15:09:42 +0200

    create datasets

--- examples/LodeSTAR/utils.py
+++ examples/LodeSTAR/utils.py
@@ -5,9 +5,16 @@ import numpy as np
 import pycocotools.mask as mask_util
 import skimage.io
 import skimage.morphology
+import tqdm
 
 
-def convert_dataset_to_coco(image_paths, mask_paths, output_dir, dataset_name):
+def convert_dataset_to_coco(
+    image_paths,
+    mask_paths,
+    output_dir,
+    dataset_name,
+    crop_slice=(slice(None), slice(None)),
+):
     """Convert dataset to COCO format.
 
     Args:
@@ -20,9 +27,25 @@ def convert_dataset_to_coco(image_paths, mask_paths, output_dir, dataset_name):
     categories = [{"id": 1, "name": "object"}]
     annotations = []
     images = []
-    for i, (image_path, mask_path) in enumerate(zip(image_paths, mask_paths)):
-        image = skimage.io.imread(image_path)
-        mask = skimage.io.imread(mask_path)
+    for i, (image_path, mask_path) in tqdm.tqdm(
+        enumerate(zip(image_paths, mask_paths)), total=len(image_paths)
+    ):
+        image_tmp = skimage.io.imread(image_path)
+        mask_tmp = skimage.io.imread(mask_path)
+
+        image = np.zeros((1010, 1010), dtype=np.uint8) + np.mean(image_tmp, dtype=int)
+        image[crop_slice] = image_tmp[crop_slice]
+
+        mask = np.zeros((1010, 1010), dtype=np.uint8)
+        mask[crop_slice] = mask_tmp[crop_slice]
+
+        # change extension to .png
+        image_path = os.path.splitext(image_path)[0] + ".png"
+
+        new_path = os.path.join(output_dir, os.path.basename(image_path))
+        # resave image
+        skimage.io.imsave(new_path, image)
+
         height, width = image.shape[:2]
 
         images.append(
@@ -41,6 +64,8 @@ def convert_dataset_to_coco(image_paths, mask_paths, output_dir, dataset_name):
             bbox = prop.bbox
             bbox = list(bbox)
             x, y, w, h = bbox
+            w = w - x
+            h = h - y
 
             rle = mask_to_coco_segmentation(mask == prop.label)
 
@@ -48,7 +73,7 @@ def convert_dataset_to_coco(image_paths, mask_paths, output_dir, dataset_name):
                 {
                     "image_id": i,
                     "category_id": 1,
-                    "bbox": [x, y, w, h],
+                    "bbox": [y, x, h, w],
                     "segmentation": rle,
                     "area": w * h,
                     "iscrowd": 0,
@@ -79,4 +104,6 @@ def mask_to_coco_segmentation(mask):
         list: COCO segmentation.
     """
     rle = mask_util.encode(np.asfortranarray(mask.astype(np.uint8)))
+    rle["counts"] = rle["counts"].decode("ascii")
+
     return rle
