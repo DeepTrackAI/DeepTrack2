fdd6e7a117ef6aee2299730a4a4c8bbb1bf63fd6
Benjamin Midtvedt <benmid@student.chalmers.se>
2022-07-26 15:28:56 +0200

    Align output region of sampletomask

--- deeptrack/features.py
+++ deeptrack/features.py
@@ -1592,7 +1592,11 @@ class SampleToMasks(Feature):
 
         output_region = kwargs["output_region"]
         output = np.zeros(
-            (output_region[2], output_region[3], kwargs["number_of_masks"])
+            (
+                output_region[2] - output_region[0],
+                output_region[3] - output_region[1],
+                kwargs["number_of_masks"],
+            )
         )
 
         for label in list_of_labels:
