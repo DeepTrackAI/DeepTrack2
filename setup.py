f32936cdd8bf33cf2cd46249193f90a6ac582f7a
Benjamin Midtvedt <benjamin.midtvedt@physics.gu.se>
2022-12-13 13:34:50 +0100

    version bump

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ if (
 
 setuptools.setup(
     name="deeptrack",  # Replace with your own username
-    version="1.6.0",
+    version="1.5.2",
     author="Benjamin Midtvedt",
     author_email="benjamin.midtvedt@physics.gu.se",
     description="A deep learning oriented microscopy image simulation package",
