81f4eda9ea3eff039e07b32debd95c807a4db043
Benjamin Midtvedt <benmid@student.chalmers.se>
2023-12-15 14:57:34 +0100

    Import torch and add Dataset class to pytorch module

--- deeptrack/pytorch/__init__.py
+++ deeptrack/pytorch/__init__.py
@@ -1,8 +1,3 @@
-try:
-    import torch
-except ImportError:
-    raise ImportError(
-        "PyTorch is not installed. "\ 
-        "Please install it consulting the documentation at https://pytorch.org/get-started/locally/ " \
-        "to access this feature."
-    )
+
+import torch
+from .data import Dataset
