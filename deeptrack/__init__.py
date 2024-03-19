88a05966390f520cb0634c776b94e799a0367989
Benjamin Midtvedt <benmid@student.chalmers.se>
2024-03-19 12:09:27 +0100

    Refactor lazy imports and remove unnecessary code

--- deeptrack/__init__.py
+++ deeptrack/__init__.py
@@ -40,33 +40,34 @@ from deeptrack.holography import *
 
 from deeptrack.image import strip
 
-if not HAS_TENSORFLOW:
-    # Lazy imports to avoid overhead of importing tensorflow
-    generators = lazy_import.lazy_module("deeptrack.generators")
-    models = lazy_import.lazy_module("deeptrack.models")
-    datasets = lazy_import.lazy_module("deeptrack.datasets")
-    losses = lazy_import.lazy_module("deeptrack.losses")
-    layers = lazy_import.lazy_module("deeptrack.layers")
-    visualization = lazy_import.lazy_module("deeptrack.visualization")
+generators = lazy_import.lazy_module("deeptrack.generators")
+models = lazy_import.lazy_module("deeptrack.models")
+datasets = lazy_import.lazy_module("deeptrack.datasets")
+losses = lazy_import.lazy_module("deeptrack.losses")
+layers = lazy_import.lazy_module("deeptrack.layers")
+visualization = lazy_import.lazy_module("deeptrack.visualization")
 
-if not HAS_TORCH:
-    pytorch = lazy_import.lazy_module("deeptrack.pytorch")
-    deeplay = lazy_import.lazy_module("deeptrack.deeplay")
+# if not HAS_TORCH:
+pytorch = lazy_import.lazy_module("deeptrack.pytorch")
+deeplay = lazy_import.lazy_module("deeptrack.deeplay")
+
+should_import = False
+if should_import:
+    from . import generators
+    from . import models
+    from . import datasets
+    from . import losses
+    from . import layers
+    from . import visualization
+    from . import pytorch
+    from . import deeplay
 
 from deeptrack import (
     image,
     utils,
     backend,
-    test,
     # Fake imports for IDE autocomplete
     # Does not actually import anything
-    pytorch,
-    generators,
-    models,
-    datasets,
-    losses,
-    layers,
-    sources,
-    visualization,
-    deeplay
 )
+
+__all__ = ()
