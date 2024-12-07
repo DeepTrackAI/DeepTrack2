23b37efbb5c81557198b9d59910c94aadcfeba9a
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2024-12-07 15:14:12 +0100

    Update core_test.py

---
+++ deeptrack/test/backend/core_test.py
@@ -0,0 +1,14 @@
+import unittest
+
+from deeptrack.backend.core import DeepTrackDataObject
+from deeptrack.backend.core import DeepTrackDataDict
+from deeptrack.backend.core import DeepTrackNode
+
+
+class TestCore(unittest.TestCase):
+    
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
