9a46bd10fc0f21634e7afd2e17fd685b817640dd
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2024-12-07 17:35:20 +0100

    Update test_core.py

--- deeptrack/test/backend/test_core.py
+++ deeptrack/test/backend/test_core.py
@@ -1,3 +1,7 @@
+# pylint: disable=C0115:missing-class-docstring
+# pylint: disable=C0116:missing-function-docstring
+# pylint: disable=C0103:invalid-name
+
 # Use this only when running the test locally.
 # import sys
 # sys.path.append(".")  # Adds the module to path.
@@ -9,8 +13,19 @@ from deeptrack.backend import core
 
 class TestCore(unittest.TestCase):
 
-    def test_trial(self):
-        self.assertTrue(1 == 1)
+    def test_DeepTrackDataObject(self):
+        dataobj = core.DeepTrackDataObject()
+        dataobj.store(1)
+        self.assertEqual(dataobj.current_value(), 1)
+        self.assertEqual(dataobj.is_valid(), True)
+
+        dataobj.invalidate()
+        self.assertEqual(dataobj.current_value(), 1)
+        self.assertEqual(dataobj.is_valid(), False)
+
+        dataobj.validate()
+        self.assertEqual(dataobj.current_value(), 1)
+        self.assertEqual(dataobj.is_valid(), True)
 
 
 if __name__ == "__main__":
