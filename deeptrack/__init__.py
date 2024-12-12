ef5c54d190f56fb29a76334eb695d9e7632fbb52
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2024-12-12 15:36:42 +0100

    Update __init__.py

--- deeptrack/__init__.py
+++ deeptrack/__init__.py
@@ -26,6 +26,24 @@ if tensorflow_installed:
 # Create a unit registry with custom pixel-related units.
 units = UnitRegistry(pint_definitions.split("\n"))
 
+'''# Check if tensorflow is installed without importing it #TBE
+import pkg_resources
+
+installed = [pkg.key for pkg in pkg_resources.working_set]
+
+if "tensorflow" in installed:
+    HAS_TENSORFLOW = True
+else:
+    HAS_TENSORFLOW = False
+
+if "torch" in installed:
+    HAS_TORCH = True
+else:
+    HAS_TORCH = False
+
+if HAS_TENSORFLOW and HAS_TORCH:
+    import torch # torch must be imported before tensorflow'''#TBE
+
 from deeptrack.features import *
 from deeptrack.aberrations import *
 from deeptrack.augmentations import *
@@ -41,12 +59,28 @@ from deeptrack.holography import *
 
 from deeptrack.image import strip
 
+# if not HAS_TENSORFLOW:
+    # Lazy imports to avoid overhead of importing tensorflow
+
+generators = lazy_import.lazy_module("deeptrack.generators")
+models = lazy_import.lazy_module("deeptrack.models")
+datasets = lazy_import.lazy_module("deeptrack.datasets")
+losses = lazy_import.lazy_module("deeptrack.losses")
+layers = lazy_import.lazy_module("deeptrack.layers")
+visualization = lazy_import.lazy_module("deeptrack.visualization")
+
 # if not HAS_TORCH:
 pytorch = lazy_import.lazy_module("deeptrack.pytorch")
 deeplay = lazy_import.lazy_module("deeptrack.deeplay")
 
 should_import = False
 if should_import:
+    from . import generators
+    from . import models
+    from . import datasets
+    from . import losses
+    from . import layers
+    from . import visualization
     from . import pytorch
     from . import deeplay
 
