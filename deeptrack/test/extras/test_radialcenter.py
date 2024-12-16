588cafb3d050529dab7698bd7be8c1c9650a445e
Alex <95913221+Pwhsky@users.noreply.github.com>
2024-12-16 19:59:19 +0100

    Documentation, formatting, unit testing (#235)
    * Update radialcenter.py
    
    Formatted code
    
    * added linebreaks
    
    * Added description and documentation for classes
    
    * linebreaks
    
    * Documentation and formatting for ImageFolder class
    
    * Update folder.py
    
    * Added test case for radialcenter
    
    * Revert "added linebreaks"
    
    This reverts commit d5c20d9b0e70ae53f0f847e4abbceb11fdf25fbf.
    
    * moved radialcenter to separate directory extras\
    
    * Added module overview, and example.
    
    * Update base.py
    
    * Update base.py
    
    * Update __init__.py
    
    * Update test_radialcenter.py
    
    * Update __init__.py
    
    * Update __init__.py
    
    * Update __init__.py
    
    * Update __init__.py
    
    * Update __init__.py
    
    * Update __init__.py
    
    * Update __init__.py
    
    * Update test_radialcenter.py
    
    ---------
    
    Co-authored-by: Alex <guslechal@student.gu.se>

---
+++ deeptrack/test/extras/test_radialcenter.py
@@ -0,0 +1,18 @@
+import unittest
+
+from deeptrack.extras import radialcenter
+
+import numpy as np
+
+
+class TestRadialCenter(unittest.TestCase):
+    def test_noise(self):
+        intensity_map = np.random.normal(0, 0.005, (100, 100))
+        x, y = radialcenter(intensity_map)
+        self.assertIsInstance(x, float)
+        self.assertIsInstance(y, float)
+        self.assertAlmostEqual(x, 50.0,delta=5)
+        self.assertAlmostEqual(y, 50.0,delta=5)
+
+if __name__ == "__main__":
+    unittest.main()
