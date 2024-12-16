4ee8451df14d4bfda1dd3f3f3e681a6e6a29aa95
Alex <95913221+Pwhsky@users.noreply.github.com>
2024-12-16 18:56:33 +0100

    Update test_radialcenter.py

--- deeptrack/test/extras/test_radialcenter.py
+++ deeptrack/test/extras/test_radialcenter.py
@@ -1,13 +1,14 @@
 import unittest
 
-from deeptrack.extras import radialcenter as rc
+from deeptrack.extras import radialcenter
 
 import numpy as np
+
 
 class TestRadialCenter(unittest.TestCase):
     def test_noise(self):
         intensity_map = np.random.normal(0, 0.005, (100, 100))
-        x, y = rc.radialcenter(intensity_map)
+        x, y = radialcenter(intensity_map)
         self.assertIsInstance(x, float)
         self.assertIsInstance(y, float)
         self.assertAlmostEqual(x, 50.0,delta=5)
