ee749505932b4283b7e1184ca2584b9464869e0e
Alex <95913221+Pwhsky@users.noreply.github.com>
2024-12-16 21:55:45 +0100

    Update test_radialcenter.py

--- deeptrack/tests/extras/test_radialcenter.py
+++ deeptrack/tests/extras/test_radialcenter.py
@@ -5,15 +5,19 @@ from deeptrack.extras import radialcenter
 
 
 class TestRadialCenter(unittest.TestCase):
-    
-    def test_noise(self):
-        intensity_map = np.random.normal(0, 0.005, (100, 100))
-        x, y = radialcenter(intensity_map)
+
+    def test_gaussian(self):
+
+        linspace = np.linspace(-10, 10, 100)
+        gaussian = np.exp(-0.5 * (
+            linspace[:, None] ** 2 + linspace[None, :] ** 2)
+        )
+        x, y = radialcenter.radialcenter(gaussian)
+
         self.assertIsInstance(x, float)
         self.assertIsInstance(y, float)
-        self.assertAlmostEqual(x, 50.0,delta=5)
-        self.assertAlmostEqual(y, 50.0,delta=5)
-
+        self.assertAlmostEqual(x, 50.0, delta=1)
+        self.assertAlmostEqual(y, 50.0, delta=1)
 
 if __name__ == "__main__":
     unittest.main()
