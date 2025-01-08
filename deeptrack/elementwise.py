81ad7c65716f442b99ac83968fe3663d9d2247dc
Alex <95913221+Pwhsky@users.noreply.github.com>
2025-01-08 15:24:32 +0100

    Added type hints

--- deeptrack/elementwise.py
+++ deeptrack/elementwise.py
@@ -115,6 +115,8 @@ Perform cosine elementwise to a Feature:
 
 """
 
+from typing import Callable, Optional, Any
+
 import numpy as np
 
 from .features import Feature
@@ -124,7 +126,12 @@ class ElementwiseFeature(Feature):
     
     __gpu_compatible__ = True
 
-    def __init__(self, function, feature=None, **kwargs):
+    def __init__(
+        self,
+        function: Callable[np.ndarray],
+        feature: Optional[Feature] = None,
+        **kwargs: Any
+    ) -> None:
         self.function = function
         super().__init__(**kwargs)
         self.feature = self.add_feature(feature) if feature else feature
@@ -138,139 +145,275 @@ class ElementwiseFeature(Feature):
         return self.function(image)
 
 
+import numpy as np
+from typing import Callable, Optional, Any
+from .features import Feature
+
+
+class ElementwiseFeature(Feature):
+    __gpu_compatible__: bool = True
+
+    def __init__(
+        self,
+        function: Callable[[np.ndarray], np.ndarray],
+        feature: Optional[Feature] = None,
+        **kwargs: Any
+    ) -> None:
+        self.function: Callable[[np.ndarray], np.ndarray] = function
+        super().__init__(**kwargs)
+        self.feature: Optional[Feature] = self.add_feature(feature) if feature else feature
+
+        if feature:
+            self.__distributed__ = False
+
+    def get(
+        self,
+        image: np.ndarray,
+        **kwargs: Any
+    ) -> np.ndarray:
+        if self.feature:
+            image = self.feature.get(image, **kwargs)
+        return self.function(image)
+
+
 class Sin(ElementwiseFeature):
-    def __init__(self, feature=None, **kwargs):
+    def __init__(
+        self,
+        feature: Optional[Feature] = None,
+        **kwargs: Any
+    ) -> None:
         super().__init__(np.sin, feature=feature, **kwargs)
 
 
 class Cos(ElementwiseFeature):
-    def __init__(self, feature=None, **kwargs):
+    def __init__(
+        self,
+        feature: Optional[Feature] = None,
+        **kwargs: Any
+    ) -> None:
         super().__init__(np.cos, feature=feature, **kwargs)
 
 
 class Tan(ElementwiseFeature):
-    def __init__(self, feature=None, **kwargs):
+    def __init__(
+        self,
+        feature: Optional[Feature] = None,
+        **kwargs: Any
+    ) -> None:
         super().__init__(np.tan, feature=feature, **kwargs)
 
 
 class Arcsin(ElementwiseFeature):
-    def __init__(self, feature=None, **kwargs):
+    def __init__(
+        self,
+        feature: Optional[Feature] = None,
+        **kwargs: Any
+    ) -> None:
         super().__init__(np.arcsin, feature=feature, **kwargs)
 
 
 class Arccos(ElementwiseFeature):
-    def __init__(self, feature=None, **kwargs):
+    def __init__(
+        self,
+        feature: Optional[Feature] = None,
+        **kwargs: Any
+    ) -> None:
         super().__init__(np.arccos, feature=feature, **kwargs)
 
 
 class Arctan(ElementwiseFeature):
-    def __init__(self, feature=None, **kwargs):
+    def __init__(
+        self,
+        feature: Optional[Feature] = None,
+        **kwargs: Any
+    ) -> None:
         super().__init__(np.arctan, feature=feature, **kwargs)
 
 
 class Sinh(ElementwiseFeature):
-    def __init__(self, feature=None, **kwargs):
+    def __init__(
+        self,
+        feature: Optional[Feature] = None,
+        **kwargs: Any
+    ) -> None:
         super().__init__(np.sinh, feature=feature, **kwargs)
 
 
 class Cosh(ElementwiseFeature):
-    def __init__(self, feature=None, **kwargs):
+    def __init__(
+        self,
+        feature: Optional[Feature] = None,
+        **kwargs: Any
+    ) -> None:
         super().__init__(np.cosh, feature=feature, **kwargs)
 
 
 class Tanh(ElementwiseFeature):
-    def __init__(self, feature=None, **kwargs):
+    def __init__(
+        self,
+        feature: Optional[Feature] = None,
+        **kwargs: Any
+    ) -> None:
         super().__init__(np.tanh, feature=feature, **kwargs)
 
 
 class Arcsinh(ElementwiseFeature):
-    def __init__(self, feature=None, **kwargs):
+    def __init__(
+        self,
+        feature: Optional[Feature] = None,
+        **kwargs: Any
+    ) -> None:
         super().__init__(np.arcsinh, feature=feature, **kwargs)
 
 
 class Arccosh(ElementwiseFeature):
-    def __init__(self, feature=None, **kwargs):
+    def __init__(
+        self,
+        feature: Optional[Feature] = None,
+        **kwargs: Any
+    ) -> None:
         super().__init__(np.arccosh, feature=feature, **kwargs)
 
 
 class Arctanh(ElementwiseFeature):
-    def __init__(self, feature=None, **kwargs):
+    def __init__(
+        self,
+        feature: Optional[Feature] = None,
+        **kwargs: Any
+    ) -> None:
         super().__init__(np.arctanh, feature=feature, **kwargs)
 
 
-
 class Round(ElementwiseFeature):
-    def __init__(self, feature=None, **kwargs):
+    def __init__(
+        self,
+        feature: Optional[Feature] = None,
+        **kwargs: Any
+    ) -> None:
         super().__init__(np.around, feature=feature, **kwargs)
 
 
 class Floor(ElementwiseFeature):
-    def __init__(self, feature=None, **kwargs):
+    def __init__(
+        self,
+        feature: Optional[Feature] = None,
+        **kwargs: Any
+    ) -> None:
         super().__init__(np.floor, feature=feature, **kwargs)
 
 
 class Ceil(ElementwiseFeature):
-    def __init__(self, feature=None, **kwargs):
+    def __init__(
+        self,
+        feature: Optional[Feature] = None,
+        **kwargs: Any
+    ) -> None:
         super().__init__(np.ceil, feature=feature, **kwargs)
 
 
 class Exp(ElementwiseFeature):
-    def __init__(self, feature=None, **kwargs):
+    def __init__(
+        self,
+        feature: Optional[Feature] = None,
+        **kwargs: Any
+    ) -> None:
         super().__init__(np.exp, feature=feature, **kwargs)
 
 
 class Log(ElementwiseFeature):
-    def __init__(self, feature=None, **kwargs):
+    def __init__(
+        self,
+        feature: Optional[Feature] = None,
+        **kwargs: Any
+    ) -> None:
         super().__init__(np.log, feature=feature, **kwargs)
 
 
 class Log10(ElementwiseFeature):
-    def __init__(self, feature=None, **kwargs):
+    def __init__(
+        self,
+        feature: Optional[Feature] = None,
+        **kwargs: Any
+    ) -> None:
         super().__init__(np.log10, feature=feature, **kwargs)
 
 
 class Log2(ElementwiseFeature):
-    def __init__(self, feature=None, **kwargs):
+    def __init__(
+        self,
+        feature: Optional[Feature] = None,
+        **kwargs: Any
+    ) -> None:
         super().__init__(np.log2, feature=feature, **kwargs)
 
 
-
 class Angle(ElementwiseFeature):
-    def __init__(self, feature=None, **kwargs):
+    def __init__(
+        self,
+        feature: Optional[Feature] = None,
+        **kwargs: Any
+    ) -> None:
         super().__init__(np.angle, feature=feature, **kwargs)
 
 
 class Real(ElementwiseFeature):
-    def __init__(self, feature=None, **kwargs):
+    def __init__(
+        self,
+        feature: Optional[Feature] = None,
+        **kwargs: Any
+    ) -> None:
         super().__init__(np.real, feature=feature, **kwargs)
 
 
 class Imag(ElementwiseFeature):
-    def __init__(self, feature=None, **kwargs):
+    def __init__(
+        self,
+        feature: Optional[Feature] = None,
+        **kwargs: Any
+    ) -> None:
         super().__init__(np.imag, feature=feature, **kwargs)
 
 
 class Abs(ElementwiseFeature):
-    def __init__(self, feature=None, **kwargs):
+    def __init__(
+        self,
+        feature: Optional[Feature] = None,
+        **kwargs: Any
+    ) -> None:
         super().__init__(np.abs, feature=feature, **kwargs)
 
 
 class Conjugate(ElementwiseFeature):
-    def __init__(self, feature=None, **kwargs):
+    def __init__(
+        self,
+        feature: Optional[Feature] = None,
+        **kwargs: Any
+    ) -> None:
         super().__init__(np.conjugate, feature=feature, **kwargs)
 
 
-
 class Sqrt(ElementwiseFeature):
-    def __init__(self, feature=None, **kwargs):
+    def __init__(
+        self,
+        feature: Optional[Feature] = None,
+        **kwargs: Any
+    ) -> None:
         super().__init__(np.sqrt, feature=feature, **kwargs)
 
 
 class Square(ElementwiseFeature):
-    def __init__(self, feature=None, **kwargs):
+    def __init__(
+        self,
+        feature: Optional[Feature] = None,
+        **kwargs: Any
+    ) -> None:
         super().__init__(np.square, feature=feature, **kwargs)
 
 
 class Sign(ElementwiseFeature):
-    def __init__(self, feature=None, **kwargs):
+    def __init__(
+        self,
+        feature: Optional[Feature] = None,
+        **kwargs: Any
+    ) -> None:
         super().__init__(np.sign, feature=feature, **kwargs)
