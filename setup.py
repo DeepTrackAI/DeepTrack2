c6afc908525799b80b90ed25e09e84db42275187
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2025-01-11 20:46:01 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as fh:
+with open("README-pypi.md", "r") as fh:
     long_description = fh.read()
 
 with open("requirements.txt", "r") as fh:
