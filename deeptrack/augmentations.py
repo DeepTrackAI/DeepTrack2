ab3ff8cfbe2c4765f15d2983217bb180ba8882c8
Alex <95913221+Pwhsky@users.noreply.github.com>
2025-03-14 13:35:37 +0100

    last image type hint

--- deeptrack/augmentations.py
+++ deeptrack/augmentations.py
@@ -378,7 +378,7 @@ class FlipLR(Augmentation):
 
     def update_properties(
         self: FlipLR,
-        image: Image,
+        image: Image | np.ndarray,
         augment: PropertyLike[bool],
         **kwargs
     ) -> None:
