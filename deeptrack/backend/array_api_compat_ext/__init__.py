2c30c558d52f7cf609b4e3cad8034025f4c77e26
BenjaminMidtvedt <41636530+BenjaminMidtvedt@users.noreply.github.com>
2025-05-24 16:39:58 +0200

    Some code cleanup and comments to make it understandable (#337)
    Co-authored-by: Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>

--- deeptrack/backend/array_api_compat_ext/__init__.py
+++ deeptrack/backend/array_api_compat_ext/__init__.py
@@ -2,4 +2,8 @@
 from array_api_compat import torch as apctorch
 from .torch import random
 
+# numpy and torch random functions are incompatible with each other.
+# The current array_api_compat module does to fix this incompatibility.
+# So we implement our own patch, which implements a numpy-compatible interface
+# for the torch random functions.
 apctorch.random = random
