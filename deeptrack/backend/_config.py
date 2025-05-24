20132c6827f852ba110ba8a8e0e0692114f1138b
Benjamin Midtvedt <benmid@student.chalmers.se>
2025-05-24 15:44:44 +0200

    Some code cleanup and comments to make it understandable

--- deeptrack/backend/_config.py
+++ deeptrack/backend/_config.py
@@ -24,10 +24,30 @@ except ImportError:
 
 
 class _Proxy(types.ModuleType):
-    """Object to keep track of the current backend."""
+    """Object to keep track of the current backend, and forward calls to the correct backend.
 
-    _backend : array_api_strict
+    An instance of this object will be treated as the module `xp`. It acts like a
+    shallow wrapper around the actual backend (for example `numpy` or `torch`),
+    and forwards calls to the correct backend.
+
+    This is especially useful for array creation functions, to ensure that the correct
+    array type is created.
+
+    Parameters
+    ----------
+    name : str
+        Name of the proxy object. This is used when printing the object.
+
+    Attributes
+    ----------
+    _backend : backend modukle
+        The actual backend module.
     __name__ : str
+        The name of the proxy object.
+    """
+
+    _backend: types.ModuleType
+    __name__: str
 
     def __init__(self, name: str):
         self._backend = apcnumpy
@@ -41,8 +61,15 @@ class _Proxy(types.ModuleType):
 
 
 # TODO: once intersection types are available, use them here
-xp: array_api_strict = _Proxy(__name__ + ".xp")  # Module instance  # the type is to make IDEs see this as if an array
-sys.modules[xp.__name__] = xp  # Register module name  # make the systems use this as a module
+
+# This creates the xp object, which we will use a module.
+# We assign the type to be `array_api_strict` to make IDEs see this as if it were
+# an array api module, instead of the wrapper _Proxy object.
+xp: array_api_strict = _Proxy(__name__ + ".xp")
+
+# This registers the xp object as a module. This should make import statements
+# treat xp as a module.
+sys.modules[xp.__name__] = xp
 
 
 class NullContext:
@@ -59,12 +86,24 @@ class NullContext:
 
 
 class ImageWrapperContext:
-    
-    def __enter__(_):
-        self.enable_image_wrapper()
+    """A context manager that enables the image wrapper.
 
-    def __exit__(_, *args):
-        self.disable_image_wrapper()
+    Example
+    -------
+    >>> pipeline = dt.Value(1)
+    >>> normal_result = pipeline()
+    >>> with ImageWrapperContext():
+    ...     wrapped_result = pipeline()
+    ...
+    >>> print(normal_result) # 1
+    >>> print(wrapped_result) # Image(1)
+    """
+
+    def __enter__(self, config: Config):
+        config.enable_image_wrapper()
+
+    def __exit__(self, *args):
+        config.disable_image_wrapper()
 
 
 class Config:
@@ -98,38 +137,85 @@ class Config:
         self.device = "cpu"
 
     def set_device(self, device):
+        """Set the device to use.
+
+        Can be ["cpu", "gpu", "cuda", "mps", torch.device],
+        but needs to be used with a compatible backend. Can only be "cpu"
+        if using numpy backend.
+
+        Parameters
+        ----------
+        device : str
+            The device to use.
+        """
         self.device = device
 
     def get_device(self):
+        """Get the device to use.
+
+        Returns
+        -------
+        str
+            The device to use.
+        """
         return self.device
 
     def set_backend_numpy(self):
+        """Set the backend to numpy."""
         self.set_backend("numpy")
 
     def set_backend_cupy(self):
         self.set_backend("cupy")
 
     def set_backend_torch(self):
+        """Set the backend to torch."""
         self.set_backend("torch")
 
     def set_backend(self, backend: Literal["numpy", "cupy", "torch"]):
+        """Set the backend to use.
+
+        One of ["numpy", "torch"].
+
+        Parameters
+        ----------
+        backend : str
+            The backend to use.
+        """
         self.backend = backend
         xp._backend = importlib.import_module(f"array_api_compat.{backend}")
 
     def get_backend(self):
+        """Get the current backend."""
         return self.backend
 
     def disable_image_wrapper(self):
+        """Disable the image wrapper.
+
+        This will ensure that `Image` objects are not used."""
         self.image_wrapper = False
 
     def enable_image_wrapper(self):
+        """Enable the image wrapper.
+
+        This will ensure that `Image` objects are used."""
         self.image_wrapper = True
 
     def wrapper_enabled_context(self):
+        """Return a context manager that enables the image wrapper.
 
-        return ImageWrapperContext() if not self.image_wrapper else NullContext()
+        This will ensure that `Image` objects are used.
 
-    def with_backend(self, backend: Literal["numpy", "cupy", "torch"]):
+        Examples
+        --------
+        >>> pipeline = dt.Value(1)
+        >>> with config.wrapper_enabled_context():
+        ...     result = pipeline()
+        >>> print(result) # Image(1)
+        """
+        return ImageWrapperContext(self) if not self.image_wrapper else NullContext()
+
+    def with_backend(self, backend: Literal["numpy", "torch"]):
+        """Return a context manager that changes the backend."""
         current_backend = self.backend
         if current_backend == backend:
             return NullContext()
