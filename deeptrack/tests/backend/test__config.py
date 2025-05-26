da03617cb2a0dd9a9692dc354301b2b26f732741
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2025-05-26 19:38:27 +0200

    Update test__config.py

--- deeptrack/tests/backend/test__config.py
+++ deeptrack/tests/backend/test__config.py
@@ -36,6 +36,53 @@ class TestCore(unittest.TestCase):
             array = xp.arange(5)
             self.assertIsInstance(array, np.ndarray)
 
+    def test__Proxy_get_float_dtype(self):
+
+        from array_api_compat import numpy as apc_np
+
+        xp = _config._Proxy("numpy")
+        xp.set_backend(apc_np)
+
+        # Test default float dtype (NumPy)
+        dtype_default = xp.get_float_dtype()
+        self.assertIn(
+            dtype_default,
+            ("float64", "numpy.float64"),  # API compat may return either
+        )
+
+        # Test explicit float32
+        dtype_32 = xp.get_float_dtype("float32")
+        self.assertIn(dtype_32, ("float32", "numpy.float32"))
+
+        # Test explicit float64
+        dtype_32 = xp.get_float_dtype("float64")
+        self.assertIn(dtype_32, ("float64", "numpy.float64"))
+
+        if _config.TORCH_AVAILABLE:
+            from array_api_compat import torch as apc_torch
+
+            xp.set_backend(apc_torch)
+
+            # Test default float dtype (PyTorch)
+            dtype_default = xp.get_float_dtype()
+            self.assertIn(
+                str(dtype_default),
+                ("float32", "torch.float32"),
+            )
+
+            # Test explicit float32
+            dtype_32 = xp.get_float_dtype("float32")
+            self.assertIn(
+                str(dtype_32),
+                ("float32", "torch.float32"),
+            )
+
+            # Switch bact to NumPy.
+            xp.set_backend(apc_np)
+
+            dtype_default = xp.get_float_dtype()
+            self.assertIn(dtype_default, ("float64", "numpy.float64"))
+
 
 if __name__ == "__main__":
     unittest.main()
