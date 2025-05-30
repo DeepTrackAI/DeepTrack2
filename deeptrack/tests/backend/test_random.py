b24081edcd1ca31394eea49d8cf63a5fcecc3fc3
Alex <95913221+Pwhsky@users.noreply.github.com>
2025-05-30 14:14:39 +0200

    test_random: mean test and numpy comparison

--- deeptrack/tests/backend/test_random.py
+++ deeptrack/tests/backend/test_random.py
@@ -1,27 +1,22 @@
 import unittest
 
-import torch
-
+import numpy as np
 from deeptrack.backend.array_api_compat_ext.torch import random
 
 
-class TestRandom(unittest.TestCase):
-  def test_rand(self):
-    shapes = [(2, ) , (3, 4) ]
-    dtypes = [torch.float32, torch.float64]
-    devices = [torch.device("cpu"), "cpu"]
-    for i in range(len(shapes)):
-        shape = shapes[i]
-        dtype = dtypes[i]
-        device = devices[i]
-        
-        torch.manual_seed(1)
-        expected = torch.rand(*shape, dtype=dtype, device=device)
+class TestRandomNumpy(unittest.TestCase):
+    def test_rand(self):
+        shapes = [(2, ), (3, 4)]
+        dtypes = [torch.float32, torch.float64]
+        devices = [torch.device("cpu"), "cpu"]
 
-        torch.manual_seed(1)
-        generated = random.rand(*shape, dtype=dtype, device=device)
-        
-        self.assertEqual(generated.shape, expected.shape)
-        self.assertEqual(generated.dtype, expected.dtype)
-        self.assertEqual(generated.device, expected.device)
-        self.assertTrue(torch.equal(generated, expected))
+        for shape, dtype, device in zip(shapes, dtypes, devices):
+
+            expected = np.random.rand(*shape)
+            generated = rand(*shape, dtype=dtype, device=device)
+            self.assertEqual(generated.shape, expected.shape)
+            self.assertEqual(generated.dtype, dtype)
+
+        a = rand(100, dtype=torch.float32, device="cpu")
+        b = np.random.rand(100)
+        self.assertAlmostEqual(a.mean(), np.mean(b), delta = 1)
