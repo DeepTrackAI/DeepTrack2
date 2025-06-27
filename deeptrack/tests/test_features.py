1cf1973736544e9585678e0945161fe43c4e7b41
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2025-06-27 11:21:13 +0200

    Update test_features.py

--- deeptrack/tests/test_features.py
+++ deeptrack/tests/test_features.py
@@ -1238,43 +1238,45 @@ class TestFeatures(unittest.TestCase):
 
 
     def test_Slice_constant(self):
-        input = np.arange(9).reshape((3, 3))
+        image = np.arange(9).reshape((3, 3))
 
         A = features.DummyFeature()
+
         A0 = A[0]
+        a0 = A0.resolve(image)
+        self.assertEqual(a0.tolist(), image[0].tolist())
+
         A1 = A[1]
+        a1 = A1.resolve(image)
+        self.assertEqual(a1.tolist(), image[1].tolist())
+
         A22 = A[2, 2]
+        a22 = A22.resolve(image)
+        self.assertEqual(a22, image[2, 2])
+
         A12 = A[1, lambda: -1]
-
-        a0 = A0.resolve(input)
-        a1 = A1.resolve(input)
-        a22 = A22.resolve(input)
-        a12 = A12.resolve(input)
-
-        self.assertEqual(a0.tolist(), input[0].tolist())
-        self.assertEqual(a1.tolist(), input[1].tolist())
-        self.assertEqual(a22, input[2, 2])
-        self.assertEqual(a12, input[1, -1])
+        a12 = A12.resolve(image)
+        self.assertEqual(a12, image[1, -1])
 
     def test_Slice_colon(self):
-
         input = np.arange(16).reshape((4, 4))
 
         A = features.DummyFeature()
 
         A0 = A[0, :1]
-        A1 = A[1, lambda: 0 : lambda: 4 : lambda: 2]
-        A2 = A[lambda: slice(0, 4, 1), 2]
-        A3 = A[lambda: 0 : lambda: 2, :]
-
         a0 = A0.resolve(input)
-        a1 = A1.resolve(input)
-        a2 = A2.resolve(input)
-        a3 = A3.resolve(input)
-
         self.assertEqual(a0.tolist(), input[0, :1].tolist())
+
+        A1 = A[1, lambda: 0 : lambda: 4 : lambda: 2]
+        a1 = A1.resolve(input)
         self.assertEqual(a1.tolist(), input[1, 0:4:2].tolist())
+
+        A2 = A[lambda: slice(0, 4, 1), 2]
+        a2 = A2.resolve(input)
         self.assertEqual(a2.tolist(), input[:, 2].tolist())
+
+        A3 = A[lambda: 0 : lambda: 2, :]
+        a3 = A3.resolve(input)
         self.assertEqual(a3.tolist(), input[0:2, :].tolist())
 
     def test_Slice_ellipse(self):
@@ -1284,18 +1286,19 @@ class TestFeatures(unittest.TestCase):
         A = features.DummyFeature()
 
         A0 = A[..., :1]
-        A1 = A[..., lambda: 0 : lambda: 4 : lambda: 2]
-        A2 = A[lambda: slice(0, 4, 1), ...]
-        A3 = A[lambda: 0 : lambda: 2, lambda: ...]
-
         a0 = A0.resolve(input)
-        a1 = A1.resolve(input)
-        a2 = A2.resolve(input)
-        a3 = A3.resolve(input)
-
         self.assertEqual(a0.tolist(), input[..., :1].tolist())
+
+        A1 = A[..., lambda: 0 : lambda: 4 : lambda: 2]
+        a1 = A1.resolve(input)
         self.assertEqual(a1.tolist(), input[..., 0:4:2].tolist())
+
+        A2 = A[lambda: slice(0, 4, 1), ...]
+        a2 = A2.resolve(input)
         self.assertEqual(a2.tolist(), input[:, ...].tolist())
+
+        A3 = A[lambda: 0 : lambda: 2, lambda: ...]
+        a3 = A3.resolve(input)
         self.assertEqual(a3.tolist(), input[0:2, ...].tolist())
 
     def test_Slice_static_dynamic(self):
