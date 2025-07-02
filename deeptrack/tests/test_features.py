e2e83218b4a03c77846010e004cdf4eb62e61888
Alex <95913221+Pwhsky@users.noreply.github.com>
2025-07-02 14:16:15 +0200

    unit tests: channelfirst2d

--- deeptrack/tests/test_features.py
+++ deeptrack/tests/test_features.py
@@ -1953,10 +1953,18 @@ class TestFeatures(unittest.TestCase):
         output_image = channel_first_feature.get(input_image_2d, axis=-1)
         self.assertEqual(output_image.shape, (1, 10, 20))
 
+        input_image_2d = torch.rand(10, 20)
+        output_image = channel_first_feature.get(input_image_2d, axis=-1)
+        self.assertEqual(tuple(output_image.shape), (1, 10, 20))
+
         input_image_3d = np.random.rand(10, 20, 3)
         output_image = channel_first_feature.get(input_image_3d, axis=-1)
         self.assertEqual(output_image.shape, (3, 10, 20))
 
+        input_image_3d = torch.rand(10, 20, 3)
+        output_image_3d = channel_first_feature.get(input_image_3d, axis=-1)
+        self.assertEqual(tuple(output_image_3d.shape), (3, 10, 20))
+        
 
     def test_Upscale(self):
         microscope = optics.Fluorescence(output_region=(0, 0, 32, 32))
