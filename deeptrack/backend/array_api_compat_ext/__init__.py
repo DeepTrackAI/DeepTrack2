fa30c97e6efcaaae3d579fa923ea4101836280f9
Alex <95913221+Pwhsky@users.noreply.github.com>
2025-07-03 13:54:39 +0200

    Update __init__.py

--- deeptrack/backend/array_api_compat_ext/__init__.py
+++ deeptrack/backend/array_api_compat_ext/__init__.py
@@ -1,7 +1,7 @@
 from deeptrack import TORCH_AVAILABLE
 if TORCH_AVAILABLE:
   from array_api_compat import torch as apctorch
-from deeptrack.backend.array_api_compat_ext.torch import random
+  from deeptrack.backend.array_api_compat_ext.torch import random
 
 # NumPy and PyTorch random functions are incompatible with each other.
 # The current array_api_compat module does not fix this incompatibility.
