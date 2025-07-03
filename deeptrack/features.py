e648f9fc3c4bab3b01406f14be8151a100a90f59
Alex <95913221+Pwhsky@users.noreply.github.com>
2025-07-03 11:44:51 +0200

    Uses input(_ID) instead of input.current_value(_ID)
    This fixed the issue of not being able to call the feature with ().

--- deeptrack/features.py
+++ deeptrack/features.py
@@ -651,7 +651,7 @@ class Feature(DeepTrackNode):
             ):
                 self._input.set_value(image_list, _ID=_ID)
 
-            if image_list is None and self._input.current_value(_ID=_ID) is not None:
+            if image_list is None and self._input(_ID=_ID) is not None:
                 self._input.set_value(None, _ID=_ID)
 
             # A dict to store values of self.arguments before updating them.
