0e8aa7c34c91e9c4db0b8dbfcb20b9883b9cbe4e
Alex <95913221+Pwhsky@users.noreply.github.com>
2025-07-03 13:15:50 +0200

    check torch availability in tests

--- deeptrack/tests/backend/test_random.py
+++ deeptrack/tests/backend/test_random.py
@@ -1,8 +1,11 @@
 import unittest
 
 import numpy as np
-import torch
+
+from deeptrack.backend import TORCH_AVAILABLE 
 from deeptrack.backend.array_api_compat_ext.torch import random
+
+
 """
 TODO: Implement tests for all of these functions to start with.
     "rand",
@@ -22,18 +25,19 @@ TODO: Implement tests for all of these functions to start with.
 """
 
 class TestRandom(unittest.TestCase):
-    def test_rand(self):
-        shapes = [(2, ), (3, 4)]
-        dtypes = [torch.float32, torch.float64]
-        devices = [torch.device("cpu"), "cpu"]
-
-        for shape, dtype, device in zip(shapes, dtypes, devices):
-
-            expected = np.random.rand(*shape)
-            generated = random.rand(*shape, dtype=dtype, device=device)
-            self.assertEqual(generated.shape, expected.shape)
-            self.assertEqual(generated.dtype, dtype)
-
-        a = random.rand(100, dtype=torch.float32, device="cpu")
-        b = np.random.rand(100)
-        self.assertAlmostEqual(a.mean(), np.mean(b), delta=1)  # Use a different rand
+    if TORCH_AVAILABLE:
+        def test_rand(self):
+            shapes = [(2, ), (3, 4)]
+            dtypes = [torch.float32, torch.float64]
+            devices = [torch.device("cpu"), "cpu"]
+    
+            for shape, dtype, device in zip(shapes, dtypes, devices):
+    
+                expected = np.random.rand(*shape)
+                generated = random.rand(*shape, dtype=dtype, device=device)
+                self.assertEqual(generated.shape, expected.shape)
+                self.assertEqual(generated.dtype, dtype)
+    
+            a = random.rand(100, dtype=torch.float32, device="cpu")
+            b = np.random.rand(100)
+            self.assertAlmostEqual(a.mean(), np.mean(b), delta=1)  # Use a different rand
