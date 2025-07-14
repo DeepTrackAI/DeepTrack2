d0c612e0b36320ce7f71bd4e721aa76f35157f34
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2025-07-14 20:38:51 +0100

    Update test_math.py

--- deeptrack/tests/test_math.py
+++ deeptrack/tests/test_math.py
@@ -12,17 +12,17 @@ from deeptrack.backend import OPENCV_AVAILABLE, TORCH_AVAILABLE, xp
 from deeptrack.tests import BackendTestBase
 
 
-class TestMathNumpy(BackendTestBase):
+class TestMath_Numpy(BackendTestBase):
     BACKEND = "numpy"
 
     def test_Average(self):
-        expected_shape = (10, 30, 20)
         input_image0 = xp.ones((10, 30, 20)) * 2
         input_image1 = xp.ones((10, 30, 20)) * 4
         feature = math.Average(axis=0)
         average = feature.resolve([input_image0, input_image1])
         self.assertTrue(xp.all(average == 3), True)
-        self.assertEqual(average.shape, expected_shape)
+        self.assertEqual(average.shape, (10, 30, 20))
+
 
     def test_Clip(self):
         input_image = xp.asarray(np.array([[10, 4], [4, -10]]))
@@ -38,6 +38,7 @@ class TestMathNumpy(BackendTestBase):
         clipped_feature = feature.resolve(input_image)
         self.assertTrue(xp.all(clipped_feature == expected_output_2))
 
+
     def test_NormalizeMinMax(self):
         input_image = xp.asarray(np.array([[10, 4], [4, -10]]))
         expected_output = xp.asarray(np.array([[5, 2], [2, -5]]))
@@ -46,12 +47,14 @@ class TestMathNumpy(BackendTestBase):
         normalized_image = feature.resolve(input_image)
         self.assertTrue(xp.all(normalized_image == expected_output))
 
+
     def test_NormalizeStandard(self):
         feature = math.NormalizeStandard()
         input_image = xp.asarray(np.array([[1, 2], [3, 4]], dtype=float))
         normalized_image = feature.resolve(input_image)
         self.assertEqual(xp.mean(normalized_image), 0)
         self.assertEqual(xp.std(normalized_image), 1)
+
 
     def test_Blur(self):
         # TODO: check this test with torch
@@ -66,7 +69,7 @@ class TestMathNumpy(BackendTestBase):
 
 # Extending the test and setting the backend to torch
 @unittest.skipUnless(TORCH_AVAILABLE, "PyTorch is not installed.")
-class TestMathTorch(TestMathNumpy):
+class TestMath_Torch(TestMath_Numpy):
     BACKEND = "torch"
     pass
 
