34e02a45e963d0c2c8fb0fb1d5782db64e9469af
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2025-07-15 16:43:08 +0100

    Gv/final/math (#386)
    * Update __init__.py
    
    * Update test_math.py
    
    * Update math.py
    
    * Update math.py
    
    * Update test_math.py
    
    * Update math.py
    
    * Update math.py
    
    * Update test_math.py
    
    * Update math.py
    
    * Update math.py
    
    * Update math.py
    
    * Update test_math.py
    
    * Update math.py
    
    * Update math.py
    
    * Update math.py
    
    * Update math.py
    
    * Update math.py
    
    * Update math.py
    
    * Update test_math.py
    
    * Update math.py

--- deeptrack/tests/test_math.py
+++ deeptrack/tests/test_math.py
@@ -4,6 +4,7 @@
 
 import unittest
 
+import array_api_compat as apc
 import numpy as np
 from scipy.ndimage import uniform_filter
 
@@ -11,47 +12,65 @@ from deeptrack import math
 from deeptrack.backend import OPENCV_AVAILABLE, TORCH_AVAILABLE, xp
 from deeptrack.tests import BackendTestBase
 
+if TORCH_AVAILABLE:
+    import torch
 
-class TestMathNumpy(BackendTestBase):
+
+class TestMath_Numpy(BackendTestBase):
     BACKEND = "numpy"
 
     def test_Average(self):
-        expected_shape = (10, 30, 20)
         input_image0 = xp.ones((10, 30, 20)) * 2
         input_image1 = xp.ones((10, 30, 20)) * 4
         feature = math.Average(axis=0)
         average = feature.resolve([input_image0, input_image1])
         self.assertTrue(xp.all(average == 3), True)
-        self.assertEqual(average.shape, expected_shape)
+        self.assertEqual(average.shape, (10, 30, 20))
+
 
     def test_Clip(self):
-        input_image = xp.asarray(np.array([[10, 4], [4, -10]]))
-        expected_output_1 = xp.asarray(np.array([[5, 4], [4, -5]]))
-        expected_output_2 = xp.asarray(np.array([[5, 6], [7, 8]]))
-
+        input_image = xp.asarray([[10, 4], [4, -10]])
         feature = math.Clip(min=-5, max=5)
         clipped_feature = feature.resolve(input_image)
-        self.assertTrue(xp.all(clipped_feature == expected_output_1))
+        self.assertTrue(
+            xp.all(clipped_feature == xp.asarray([[5, 4], [4, -5]]))
+        )
 
         input_image = xp.asarray(np.array([[5, 6], [7, 8]]))
         feature = math.Clip(min=0, max=10)
         clipped_feature = feature.resolve(input_image)
-        self.assertTrue(xp.all(clipped_feature == expected_output_2))
+        self.assertTrue(
+            xp.all(clipped_feature == xp.asarray([[5, 6], [7, 8]]))
+        )
+
 
     def test_NormalizeMinMax(self):
-        input_image = xp.asarray(np.array([[10, 4], [4, -10]]))
-        expected_output = xp.asarray(np.array([[5, 2], [2, -5]]))
-
+        input_image = xp.asarray([[10, 4], [4, -10]])
         feature = math.NormalizeMinMax(min=-5, max=5)
         normalized_image = feature.resolve(input_image)
-        self.assertTrue(xp.all(normalized_image == expected_output))
+        self.assertTrue(
+            xp.all(normalized_image == xp.asarray([[5, 2], [2, -5]]))
+        )
+
 
     def test_NormalizeStandard(self):
+        input_image = xp.asarray([[1, 2], [3, 4]], dtype=float)
         feature = math.NormalizeStandard()
-        input_image = xp.asarray(np.array([[1, 2], [3, 4]], dtype=float))
         normalized_image = feature.resolve(input_image)
         self.assertEqual(xp.mean(normalized_image), 0)
-        self.assertEqual(xp.std(normalized_image), 1)
+        if apc.is_torch_array(normalized_image):
+            # By default, torch.std() is unbiased, i.e., divides by N-1
+            self.assertEqual(torch.std(normalized_image, unbiased=False), 1)
+        else:
+            self.assertEqual(xp.std(normalized_image), 1)
+
+
+    def test_NormalizeQuantile(self):
+        input_image = xp.asarray([[1, 2], [3, 100]], dtype=float)
+        feature = math.NormalizeQuantile(quantiles=(0.25, 0.75))
+        output = feature.resolve(input_image)
+        self.assertAlmostEqual(xp.quantile(output, 0.5), 0, places=5)
+
 
     def test_Blur(self):
         # TODO: check this test with torch
@@ -66,7 +85,7 @@ class TestMathNumpy(BackendTestBase):
 
 # Extending the test and setting the backend to torch
 @unittest.skipUnless(TORCH_AVAILABLE, "PyTorch is not installed.")
-class TestMathTorch(TestMathNumpy):
+class TestMath_Torch(TestMath_Numpy):
     BACKEND = "torch"
     pass
 
@@ -101,12 +120,6 @@ class TestMath(unittest.TestCase):
         feature = math.MinPooling(ksize=2)
         pooled_image = feature.resolve(input_image)
         self.assertTrue(np.all(pooled_image == [[1, 3]]))
-
-    def test_NormalizeQuantile(self):
-        input_image = np.array([[1, 2], [3, 100]], dtype=float)
-        feature = math.NormalizeQuantile(quantiles=(0.25, 0.75))
-        output = feature.resolve(input_image)
-        self.assertAlmostEqual(np.median(output), 0, places=5)
 
     def test_MedianBlur(self):
         input_image = np.random.rand(32, 32)
