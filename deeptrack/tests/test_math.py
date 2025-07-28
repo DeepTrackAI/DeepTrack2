47711838f107e46cacf7298225c9f4d7feb1d34a
Alex <95913221+Pwhsky@users.noreply.github.com>
2025-07-28 14:05:45 +0200

    u

--- deeptrack/tests/test_math.py
+++ deeptrack/tests/test_math.py
@@ -113,7 +113,7 @@ class TestMath(unittest.TestCase):
             input_image = torch.tensor([[[[1.0, 2.0, 3.0, 4.0],
                                           [5.0, 6.0, 7.0, 8.0]]]])
             feature = math.AveragePooling(ksize=2)
-            pooled_image = feature(input_image)
+            pooled_image = feature.get(input_image)
             self.assertTrue(torch.allclose(pooled_image, torch.tensor([[[[3.5, 5.5]]]])))
 
     def test_MaxPooling(self):
