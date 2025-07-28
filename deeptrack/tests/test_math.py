46fb8739ff0acdab2a1eae23b30b4e4aae05fe7e
Alex <95913221+Pwhsky@users.noreply.github.com>
2025-07-28 14:53:15 +0200

    u

--- deeptrack/tests/test_math.py
+++ deeptrack/tests/test_math.py
@@ -113,7 +113,7 @@ class TestMath(unittest.TestCase):
             input_image = torch.tensor([[[ [1.0, 2.0, 3.0, 4.0],
                                            [5.0, 6.0, 7.0, 8.0] ]]])
             feature = math.AveragePooling(ksize=2)
-            pooled_image = feature.get(input_image, ksize=2)
+            pooled_image = feature(input_image, ksize=2)
             
             expected = torch.tensor([[[[3.5, 5.5]]]])
             self.assertEqual(pooled_image.shape, expected.shape)
