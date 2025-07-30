5b94e5a0666d58f85ae7e6ed23234366b562fd95
Alex <95913221+Pwhsky@users.noreply.github.com>
2025-07-30 10:16:03 +0200

    attempt at fixing broken optics

--- deeptrack/optics.py
+++ deeptrack/optics.py
@@ -155,6 +155,8 @@ from deeptrack.features import DummyFeature, Feature, StructuralFeature
 from deeptrack.image import Image, pad_image_to_fft, maybe_cupy
 from deeptrack.types import ArrayLike, PropertyLike
 
+import torch
+
 from deeptrack import image
 from deeptrack import units_registry as u
 
@@ -358,6 +360,8 @@ class Microscope(StructuralFeature):
             )
 
             imaged_sample = self._objective.resolve(sample_volume)
+            if self.get_backend() == "torch": 
+                imaged_sample = torch.from_numpy(imaged_sample)
 
         # Upscale given by the optics needs to be handled separately.
         if _upscale_given_by_optics != (1, 1, 1):
