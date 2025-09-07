16e52ecc36d4b3927c20b44882ae5a539157a5a1
Alex <95913221+Pwhsky@users.noreply.github.com>
2025-09-07 12:39:02 +0200

    u

--- deeptrack/tests/test_noises.py
+++ deeptrack/tests/test_noises.py
@@ -34,7 +34,6 @@ class TestNoises_Numpy(BackendTestBase):
         input_image = Image(xp.zeros((256, 256)))
         output_image = noise.resolve(input_image)
 
-        self.assertIsInstance(type(output_image), type(input_image))
         self.assertEqual(output_image.shape, (256, 256))
         self.assertTrue(xp.all(xp.array(output_image) == 0.5))
 
