9dbd195de5745263898cd05db1461cad0289ba09
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2025-09-08 11:11:03 +0200

    Gv/polishing001 (#422)
    * Update core.py
    
    * Update DTAT399A_backend.core.ipynb
    
    * Update DTDV411_style.ipynb
    
    * Update core.py
    
    * Update test_core.py
    
    * Update DTDV401_overview.ipynb
    
    * Update DTDV401_overview.ipynb
    
    * Update DTAT306_properties.ipynb
    
    * Update properties.py
    
    * Update scatterers.py
    
    * Update units.py

--- deeptrack/tests/backend/test_core.py
+++ deeptrack/tests/backend/test_core.py
@@ -1,7 +1,7 @@
 # pylint: disable=C0115:missing-class-docstring
 # pylint: disable=C0116:missing-function-docstring
 # pylint: disable=C0103:invalid-name
-
+ 
 # Use this only when running the test locally.
 # import sys
 # sys.path.append(".")  # Adds the module to path.
