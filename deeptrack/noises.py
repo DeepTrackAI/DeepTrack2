c4b0c7265e79e019a72a7f683ec63f455252c041
mirjagranfors <mirja98granfors@gmail.com>
2025-09-15 12:42:38 +0200

    update tutorial

--- deeptrack/noises.py
+++ deeptrack/noises.py
@@ -217,7 +217,11 @@ class Gaussian(Noise):
 
         # For a Torch backend.
         elif self.get_backend() == "torch":
-            noisy_image = mu + image + torch.randn(*image.shape, device=image.device) * sigma
+            noisy_image = (
+                mu
+                + image
+                + torch.randn(*image.shape, device=image.device) * sigma
+            )
 
         return noisy_image
 
