ea5e02520339b507ec3c261ce31ab35a4ef96b9f
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2026-02-15 00:26:08 +0800

    Update test_radialcenter.py

--- deeptrack/tests/extras/test_radialcenter.py
+++ deeptrack/tests/extras/test_radialcenter.py
@@ -1,3 +1,11 @@
+# pylint: disable=C0115:missing-class-docstring
+# pylint: disable=C0116:missing-function-docstring
+# pylint: disable=C0103:invalid-name
+ 
+# Use this only when running the test locally.
+# import sys
+# sys.path.append(".")  # Adds the module to path.
+
 import unittest
 
 import numpy as np
@@ -12,8 +20,8 @@ class TestRadialCenter(unittest.TestCase):
         x, y = radialcenter(intensity_map)
         self.assertIsInstance(x, float)
         self.assertIsInstance(y, float)
-        self.assertAlmostEqual(x, 50.0,delta=5)
-        self.assertAlmostEqual(y, 50.0,delta=5)
+        self.assertAlmostEqual(x, 50.0, delta=5)
+        self.assertAlmostEqual(y, 50.0, delta=5)
 
 
 if __name__ == "__main__":
